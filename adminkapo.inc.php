6e87db8a3ab3c4d49f62c5b77b7e62dcdda304f5
axel584 <axel584@gmail.com>
2016-05-12 21:39:47 +0200

    correction de quelques variables

--- adminkapo.inc.php
+++ adminkapo.inc.php
@@ -1,5 +1,6 @@
 <?php
 //include_once $vojo."util.php";
+if (!isset($vojo)) { $vojo="";}
 $url=$_SERVER['REQUEST_URI'];
 $pagxo=explode("/", $_SERVER["SCRIPT_NAME"]);
 $subjekto=$pagxo[count($pagxo)-1];
@@ -33,47 +34,47 @@ echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
 * menu personnel 
 *
 -->
-<? if ($persono_id!="") { ?>
+<?php if ($persono_id!="") { ?>
 	<div id="menuo">
 		<table>
 			<tr><td class="unua">
 					<h1><?=$enirnomo?></h1>
-					<p><a href="#"><a href="<?php echo $vojo."eliri.php"?>"><?=$lgv_eliru?></a></p>
+					<p><a href="#"><a href="<?php echo $vojo."eliri.php"?>">Fermer la session</a></p>
 				</td>
 				<td>
 					<h2><?=$lgv_persona?></h2>
 					<p><a href="<?php echo $vojo."forum/index.php?sid=".$phpbb2mysql_sid;?>">
-					<? echo $lgv_forumo." ".phpbb_nombre_nouveau_message($persono_id);?>
+					<?php //$lgv_forumo." ".phpbb_nombre_nouveau_message($persono_id);?>
 					</a></p>
 					<p><a href="javascript:void(0);" onClick="window.open('<?=$vojo?>forum/chatbox_mod/chatbox.php','12_ChatBox','scrollbars=no,width=700,height=550')">Discussion en ligne</a></p>
 					<p><a href="<?php echo $vojo."personinformoj.php";?>"><?=$lgv_sxangipersoninformojn?></a></p>
 				</td>
-			<? if ($rajto=="K" || $rajto=="A"){ ?>
+			<?php if ($rajto=="K" || $rajto=="A"){ ?>
 				<td>
 					<h2><?=$lgv_korektanta?></h2>
 					<p><a href="<?php echo $vojo."statLauxKategorio.php";?>"><?=$lgv_statistiko?></a></p>
 					<p><a href="<?php echo $vojo."helppagxo.php";?>"><?=$lgv_helppagxo?></a></p>
 					<p><a href="<?php echo $vojo."miajlernantoj.php";?>"><?=$lgv_zorgistudantojn?></a></p>
 				</td>
-			<? } ?>
-			<? if ($rajto=="I"){ ?>
+			<?php } ?>
+			<?php if ($rajto=="I"){ ?>
 				<td class="elemento">
 					<h2><?=$lgv_administranta?></h2>
 					<p><a href="#"><?=$lgv_statistiko?></a></p>
 					<p><a href="<?php echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
 				</td>
-			<? } ?>
-			<? if ($rajto=="A"){ ?>
+			<?php } ?>
+			<?php if ($rajto=="A"){ ?>
 				<td class="elemento">
 					<h2><?=$lgv_administranta?></h2>
 					<p><a href="<?php echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
 					<p><a href="<?php echo $vojo."administri.php";?>"><?=$lgv_zorgiuzantojn?></a></p>
 				</td>
-			<? } ?>
+			<?php } ?>
 			</tr>
 		</table>
 	</div>
-<? } ?>
+<?php } ?>
 <!-- 
 *
 * page = menu lateral + onglets (facultatifs) + contenu 

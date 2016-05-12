7eeb3a73209dabe714dd068c001c3db2d49b80eb
axel584 <axel584@gmail.com>
2016-05-12 21:30:05 +0200

    suppression des .bak

--- adminkapo.inc.php
+++ adminkapo.inc.php
@@ -1,16 +1,12 @@
 <?php
 //include_once $vojo."util.php";
-$url=$_SERVER["SCRIPT_URI"];
+$url=$_SERVER['REQUEST_URI'];
 $pagxo=explode("/", $_SERVER["SCRIPT_NAME"]);
 $subjekto=$pagxo[count($pagxo)-1];
-$metodo=$_GET["metodo"];
-if ($metodo=="") {$metodo="U";}
-$style=$_GET["style"];
+$metodo=isset($_GET["metodo"])?$_GET["metodo"]:"U";
+$style=isset($_GET["style"])?$_GET["style"]:"";
 if ($style==""){
-	$style=$_COOKIE["style"];
-	if ($style==""){
-		$style="bleu";
-	}
+	$style=isset($_COOKIE["style"])?$_COOKIE["style"]:"bleu";
 }
 setcookie("style", $style);
 ob_start("konvX");
@@ -18,7 +14,7 @@ echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
 ?>
 <html>
 <head>
-<title>ikurso-<?echo $pagxtitolo;?></title>
+<title>ikurso-<?php echo $pagxtitolo;?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link type="text/css" rel="stylesheet" href="<?=$vojo;?>style/<?=$style?>.css" title="<?=$style?>">
 
@@ -42,36 +38,36 @@ echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
 		<table>
 			<tr><td class="unua">
 					<h1><?=$enirnomo?></h1>
-					<p><a href="#"><a href="<?echo $vojo."eliri.php"?>"><?=$lgv_eliru?></a></p>
+					<p><a href="#"><a href="<?php echo $vojo."eliri.php"?>"><?=$lgv_eliru?></a></p>
 				</td>
 				<td>
 					<h2><?=$lgv_persona?></h2>
-					<p><a href="<?echo $vojo."forum/index.php?sid=".$phpbb2mysql_sid;?>">
+					<p><a href="<?php echo $vojo."forum/index.php?sid=".$phpbb2mysql_sid;?>">
 					<? echo $lgv_forumo." ".phpbb_nombre_nouveau_message($persono_id);?>
 					</a></p>
 					<p><a href="javascript:void(0);" onClick="window.open('<?=$vojo?>forum/chatbox_mod/chatbox.php','12_ChatBox','scrollbars=no,width=700,height=550')">Discussion en ligne</a></p>
-					<p><a href="<?echo $vojo."personinformoj.php";?>"><?=$lgv_sxangipersoninformojn?></a></p>
+					<p><a href="<?php echo $vojo."personinformoj.php";?>"><?=$lgv_sxangipersoninformojn?></a></p>
 				</td>
 			<? if ($rajto=="K" || $rajto=="A"){ ?>
 				<td>
 					<h2><?=$lgv_korektanta?></h2>
-					<p><a href="<?echo $vojo."statLauxKategorio.php";?>"><?=$lgv_statistiko?></a></p>
-					<p><a href="<?echo $vojo."helppagxo.php";?>"><?=$lgv_helppagxo?></a></p>
-					<p><a href="<?echo $vojo."miajlernantoj.php";?>"><?=$lgv_zorgistudantojn?></a></p>
+					<p><a href="<?php echo $vojo."statLauxKategorio.php";?>"><?=$lgv_statistiko?></a></p>
+					<p><a href="<?php echo $vojo."helppagxo.php";?>"><?=$lgv_helppagxo?></a></p>
+					<p><a href="<?php echo $vojo."miajlernantoj.php";?>"><?=$lgv_zorgistudantojn?></a></p>
 				</td>
 			<? } ?>
 			<? if ($rajto=="I"){ ?>
 				<td class="elemento">
 					<h2><?=$lgv_administranta?></h2>
 					<p><a href="#"><?=$lgv_statistiko?></a></p>
-					<p><a href="<?echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
+					<p><a href="<?php echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
 				</td>
 			<? } ?>
 			<? if ($rajto=="A"){ ?>
 				<td class="elemento">
 					<h2><?=$lgv_administranta?></h2>
-					<p><a href="<?echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
-					<p><a href="<?echo $vojo."administri.php";?>"><?=$lgv_zorgiuzantojn?></a></p>
+					<p><a href="<?php echo $vojo."stirpanelo.php";?>"><?=$lgv_stirpanelo?></a></p>
+					<p><a href="<?php echo $vojo."administri.php";?>"><?=$lgv_zorgiuzantojn?></a></p>
 				</td>
 			<? } ?>
 			</tr>

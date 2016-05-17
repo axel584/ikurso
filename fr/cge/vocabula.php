9ada0cf307090b6fa5f73eab1671e649c6c10fec
axel584 <axel584@gmail.com>
2016-05-17 14:17:40 +0200

    ajout d'isset

--- fr/cge/vocabula.php
+++ fr/cge/vocabula.php
@@ -3,8 +3,7 @@ $gxisdatigDato="2006-06-16";
 $subjekto="vocabula.php";
 $temo="listo";
 $pagxtitolo="Lexique";
-$metodo=$_GET["metodo"];
-if ($metodo=="") {$metodo="U";}
+$metodo=isset($_GET["metodo"])?$_GET["metodo"]:"U";
 include "dlekkapo.inc.php";
 ?>
 		<div class="klarigo">

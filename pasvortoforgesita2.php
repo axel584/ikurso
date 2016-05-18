0003c658ea9d5b9073c68df171cddc25bb4833bd
axel584 <axel584@gmail.com>
2016-05-18 08:18:03 +0200

    gestion des mails

--- pasvortoforgesita2.php
+++ pasvortoforgesita2.php
@@ -4,35 +4,31 @@ $retadreso=$_POST['retadreso'];
 include "util.php";
 malfermidatumbazon();
 $query = "select enirnomo,pasvorto,id from personoj where retadreso='$retadreso'";
-mysql_select_db( "ikurso");
-$result = mysql_query($query) or die (  "INSERT : Invalid query :".$query);
-if (mysql_num_rows($result)==0) {
+$result = $bdd->query($query) or die(print_r($bdd->errorInfo()));
+if (!$row = $result->fetch()) { // aucune ligne retournée
 	header("Location:pasvortoforgesita.php?erarkodo=10");
 }
 else {
-	$row = mysql_fetch_array($result);
 	// sendi mesagxon kun la forgesita pasvorto
-	$filename = "mails/pasvorto".$lingvo.".html";
+	$filename = "mails/pasvortoFR.html";
 	$fd = fopen($filename, "r");
 	$contents = fread($fd, filesize ($filename));
 	fclose($fd);
 	$contents=str_replace("##PASVORTO##",$row["pasvorto"],$contents);
 	$contents=str_replace("##ENIRNOMO##",$row["enirnomo"],$contents);
-	if (mysql_num_rows($result)>1) {
-		$row2 = mysql_fetch_array($result);
-		$contents=str_replace("##PLUS##","<b>Attention, vous avez un deuxi&egrave;me compte : ".$row2["enirnomo"]."/".$row2["pasvorto"]."<br/>Merci de contacter les administrateurs pour regler cette situation.</b>",$contents);
-	} else {
-		$contents=str_replace("##PLUS##","",$contents);
-	}
+	while ($row = $result->fetch()) { // si on récupère d'autres valeurs 
+		$contents=str_replace("##PLUS##","<b>Attention, vous avez plusieurs comptes : ".$row["enirnomo"]."/".$row["pasvorto"]."<br/>Merci de contacter les administrateurs pour regler cette situation.</b>",$contents);
+	} 
+	$contents=str_replace("##PLUS##","",$contents);
 }
 $mesagxkapo="MIME-Version: 1.0".chr(13).chr(10);
 $mesagxkapo.="Content-type: text/html;charset=utf-8".chr(13).chr(10);
 $mesagxkapo.="From: Ikurso <ikurso@esperanto-jeunes.org>".chr(13).chr(10);
+$mesagxkapo.="Reply-To: Ikurso <ikurso@esperanto-jeunes.org>".chr(13).chr(10);
 $mesagxkapo.="Date: ".date("D, j M Y H:i:s").chr(13).chr(10);
-mail($retadreso,$lgv_viaPasvorto,$contents,$mesagxkapo);
+$resultat = mail($retadreso,"Votre mot de passe pour rentrer sur I-kurso",$contents,$mesagxkapo);
 protokolo($row['id'],"PASVORTO SENDITA",$row['enirnomo']."-".$row['pasvorto']);
-
-if (mysql_num_rows($result)!=0){?> 
+?> 
 <html>
 <head>
 <title>ikurso</title>
@@ -41,12 +37,17 @@ if (mysql_num_rows($result)!=0){?>
 </head>
 <body class="popup">
 <div>
-<p><? echo $contents; ?></p>
-	<p><? echo $lgv_fermufenestron; ?></p>
-	<input class="bouton" type="button" value="<? echo $lgv_fermi; ?>" onClick="window.close();">
+<?php
+if (!$resultat) {
+	echo "<p class='eraro'>IMPOSSIBLE D'ENVOYER LE MESSAGE</p>";
+}
+?>
+<p><?php echo $contents; ?></p>
+	<p>Votre mot de passe vous a été envoyé, vous pouvez fermer cette fenêtre</p>
+	<input class="bouton" type="button" value="Fermer" onClick="window.close();">
 </div>
 </body>
 </html>
-<? } 
-mysql_free_result($result);
+<? 
+} // fin du test si des lignes sont retournées
 ?>                  

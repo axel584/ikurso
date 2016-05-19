6add72df9c42c40d57ee048ef67d78df6bfacd8f
axel584 <axel584@gmail.com>
2016-05-19 19:18:44 +0200

    suppression des variables $lgv

--- aldoniferion.php
+++ aldoniferion.php
@@ -15,32 +15,32 @@ header("Pragma: no-cache"); // HTTP/1.0
 
 // tiu funkcio konstruas la liston de cxiuj studantojn
 function listiFeriojn() {
-     global $lingvo,$persono_id,$lgv_de,$lgv_gxis,$lgv_nuligi,$lgv_aldoni;
      $i=1;
      $demando =  "select * from ferioj where persono=$persono_id";
-     mysql_select_db( "ikurso");
-     $result = mysql_query($demando) or die (  "INSERT : malbona demando :".$demando);
-     while($row = mysql_fetch_array($result)) {
+     $result = $bdd->query($demando) or die(print_r($bdd->errorInfo()));
+     while($row = $result->fetch()) {
         echo "<tr>";
-        echo "<td class=\"normala\" >".$lgv_de."</td>";
+        echo "<td class=\"normala\" >à partir du</td>";
         // ekdato
         echo "<td class=\"normala\" >&nbsp;";
         ereg("([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})", $row["ekdato"],$ekdt);
-        echo $ekdt[3]." ";
-        simplaVorto("nomo","monatoj"," where kodo='$ekdt[2]' and lingvo='$lingvo'");
-        echo " ".$ekdt[1];
+        $ekdt = explode("-",$row["ekdato"]);
+        echo $ekdt[2]." ";
+        simplaVorto("nomo","monatoj"," where kodo='$ekdt[1]' and lingvo='fr'");
+        echo " ".$ekdt[0];
         echo "&nbsp;</td>";
 
-        echo "<td class=\"normala\" >".$lgv_gxis."</td>";
+        echo "<td class=\"normala\" >jusqu’au</td>";
         // findato
         echo "<td class=\"normala\" >&nbsp;";
-        ereg("([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})", $row["findato"],$fndt);
-        echo $fndt[3]." ";
-        simplaVorto("nomo","monatoj"," where kodo='$fndt[2]' and lingvo='$lingvo'");
-        echo " ".$fndt[1];
+        //ereg("([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})", $row["findato"],$fndt);
+        $fndt = explode("-",$row["findato"]);
+        echo $fndt[2]." ";
+        simplaVorto("nomo","monatoj"," where kodo='$fndt[1]' and lingvo='fr'");
+        echo " ".$fndt[0];
         echo "&nbsp;</td>";
 
-        echo "<td class=\"normala\" ><input type=\"button\" value=\"".$lgv_nuligi."\" onClick=\"location.href='nuligiferion.php?idferion=".$row["id"]."';\"></td>";
+        echo "<td class=\"normala\" ><input type=\"button\" value=\"Supprimer\" onClick=\"location.href='nuligiferion.php?idferion=".$row["id"]."';\"></td>";
         echo "</tr>";
      }
 }
@@ -53,7 +53,7 @@ function listiFeriojn() {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" href="style.css" type="text/css">
 </head>
-<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" dir="<? echo $lgv_direkto; ?>" <? if ($validi=="jes") { echo "onLoad=\"window.alert('".$lgv_datumsavo."');\""; } ?>>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" <? if ($validi=="jes") { echo "onLoad=\"window.alert('Vos données ont été enregistrées');\""; } ?>>
 <?
 	pagxkapo();
 	menuo($persono["enirnomo"],$persono["rajtoj"]);
@@ -62,7 +62,7 @@ function listiFeriojn() {
   <table border="0" cellspacing="0" cellpadding="0">
   <tr>
   <td nowrap>
-  <div align="center" class="titolo"><? echo $lgv_zorgiferiojn; ?></div>
+  <div align="center" class="titolo">G&eacute;rer ses vacances</div>
   </td>
   </tr>
   <tr>
@@ -74,23 +74,23 @@ function listiFeriojn() {
     <form name="aldoniferion" action="aldoniferion2.php" method="POST">
   <? listiFeriojn(); 
       echo "<tr>";
-        echo "<td class=\"normala\" >".$lgv_de."</td>";
+        echo "<td class=\"normala\" >à partir du</td>";
         // ekdato
         echo "<td>";
         echo "<input type=\"text\" name=\"ekdt_tago\" size=\"3\" maxlength=\"2\">";
-konstruiMenuon("ekdt_monato","monatoj","kodo","nomo",""," where lingvo='$lingvo'");
+konstruiMenuon("ekdt_monato","monatoj","kodo","nomo",""," where lingvo='fr'");
         echo "<input type=\"text\" name=\"ekdt_jaro\" size=\"5\" maxlength=\"4\">";
         echo "</td>";
 
-        echo "<td class=\"normala\" >".$lgv_gxis."</td>";
+        echo "<td class=\"normala\" >jusqu’au</td>";
         // findato
         echo "<td>";
         echo "<input type=\"text\" name=\"findt_tago\" size=\"3\" maxlength=\"2\">";
-konstruiMenuon("findt_monato","monatoj","kodo","nomo",""," where lingvo='$lingvo'");
+konstruiMenuon("findt_monato","monatoj","kodo","nomo",""," where lingvo='fr'");
         echo "<input type=\"text\" name=\"findt_jaro\" size=\"5\" maxlength=\"4\">";
         echo "</td>";
 
-        echo "<td class=\"normala\" ><input type=\"submit\" value=\"".$lgv_aldoni."\"></td>";
+        echo "<td class=\"normala\" ><input type=\"submit\" value=\"Insérer\"></td>";
         echo "</tr>";
 ?>
   </table>

de236f902aca8779a61d226901178153d8197ecf
axel584 <axel584@gmail.com>
2016-05-20 08:27:15 +0200

    ajout du script sql

--- sercxi.php
+++ sercxi.php
@@ -1,27 +1,26 @@
-<?
+<?php
 include "util.php";
-$pagxtitolo=$lgv_sercxlisto;
-$persono_id=$_SESSION["persono_id"];
+$pagxtitolo="Résultat de la recherche";
+$persono_id=isset($_SESSION["persono_id"])?$_SESSION["persono_id"]:"";
 if ($persono_id=="") {header("Location:index.php?erarkodo=8");}
 $persono = apartigiPersonon($persono_id);
 if ($rajto!='A') {header("Location:index.php?erarkodo=4");}
-$personnomo=$_POST["personnomo"];
-$familinomo=$_POST["familinomo"];
-$celenirnomo=$_POST["celenirnomo"];
-$retadreso=$_POST["retadreso"];
-$urbo=$_POST["urbo"];
-$posxtkodo=$_POST["posxtkodo"];
-//echo "personnomo=".$personnomo." familinomo=".$familinomo." celenirnomo=".$celenirnomo." retadreso=".$retadreso." urbo=".$urbo."<br>";
+$personnomo=isset($_POST["personnomo"])?$_POST["personnomo"]:"";
+$familinomo=isset($_POST["familinomo"])?$_POST["familinomo"]:"";
+$celenirnomo=isset($_POST["celenirnomo"])?$_POST["celenirnomo"]:"";
+$retadreso=isset($_POST["retadreso"])?$_POST["retadreso"]:"";
+$urbo=isset($_POST["urbo"])?$_POST["urbo"]:"";
+$posxtkodo=isset($_POST["posxtkodo"])?$_POST["posxtkodo"]:"";
+$erarkodo = isset($_GET['erarkodo']) ? $_GET['erarkodo'] : "";
+
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D,d M Y H:i:s")." GMT");
 header("Cache-Control: no-store,no-cache, must-revalidate"); // HTTP/1.1
 header("Pragma: no-cache"); // HTTP/1.0
 
 function listiSercxadon($personnomo,$familinomo,$celenirnomo,$retadreso,$urbo,$posxtkodo) {
-    global $lingvo,$persono_id,$lgv_nekomencita,$lgv_neniuLernanto,$lgv_nuna_leciono,$lgv_adreso1,$lgv_ek,$idnoto,$lgv_familinomo;
-	$obj_persono = new personoj;
-	//$obj_nuna_kurso = new nuna_kurso;
-	$wheresql="lingvo='".$lingvo."'";
+    global $lingvo,$persono_id,$lgv_nekomencita,$lgv_neniuLernanto,$lgv_nuna_leciono,$lgv_adreso1,$lgv_ek,$idnoto,$lgv_familinomo,$bdd;
+	$wheresql="1=1";
 	if ($personnomo!="") {
 		$wheresql=$wheresql." and personnomo like '%".$personnomo."%'";
 	}
@@ -41,59 +40,58 @@ function listiSercxadon($personnomo,$familinomo,$celenirnomo,$retadreso,$urbo,$p
 		$wheresql=$wheresql." and posxtkodo like '".$posxtkodo."%'";
 	}
 	echo "<table>";
-	$personoj=$obj_persono->find($wheresql);
-	if (count($personoj)==0) {
-		echo "<tr><td>";
-		echo "<h1>".$lgv_neniuLernanto."</h1>";
-		echo "</tr></td>";
-	} /*else {
-		echo "<tr><td align=\"center\" class=\"normala\">";
-		echo $lgv_familinomo."</td>";
-		echo "<td align=\"center\" class=\"normala\">";
-		echo $lgv_adreso1."</td>";
-		echo "<td align=\"center\" class=\"normala\">";
-		echo $lgv_nuna_leciono."</td></tr>";
-	}*/
-	for($i=0;$i<count($personoj);$i++) {
+	$query="select * from personoj where ".$wheresql;
+	$result = $bdd->query($query) or die(print_r($bdd->errorInfo()));
+	
+	$nbResultat=0;
+	while($row = $result->fetch()) {
+		$nbResultat++;
 		echo "<tr";
-		if ($i%2==1) { echo " bgcolor=\"#d0d8df\"";}
+		if ($nbResultat%2==1) { echo " bgcolor=\"#d0d8df\"";}
         echo "><td>";
-        echo "(".$personoj[$i]->get_id().") ";
-		echo "<a href=\"administri.php?celpersono_id=".$personoj[$i]->get_id()."\">".$personoj[$i]->get_enirnomo()."</a>";
-        echo "&nbsp;n&eacute;(e) le ".$personoj[$i]->get_naskigxdato()."<br>";
-		echo "<i>".$personoj[$i]->get_retadreso()."</i>";
+        echo "(".$row["id"].") ";
+		echo "<a href=\"administri.php?celpersono_id=".$row["id"]."\">".$row["enirnomo"]."</a>";
+        echo "&nbsp;n&eacute;(e) le ".$row["naskigxdato"]."<br>";
+		echo "<i>".$row["retadreso"]."</i>";
         echo "</td><td>";
-        echo $personoj[$i]->get_personnomo()." ".$personoj[$i]->get_familinomo()."<br>";
-        if ($personoj[$i]->get_adreso1()!="") { echo $personoj[$i]->get_adreso1()."<br>";}
-        if ($personoj[$i]->get_adreso2()!="") {echo $personoj[$i]->get_adreso2()."<br>";}
-        echo $personoj[$i]->lando->get_kodo()."-".$personoj[$i]->get_posxtkodo()." ".$personoj[$i]->get_urbo()."<br>";
+        echo $row["personnomo"]." ".$row["familinomo"]."<br>";
+        if ($row["adreso1"]!="") { echo $row["adreso1"]."<br>";}
+        if ($row["adreso2"]!="") {echo $row["adreso2"]."<br>";}
+        echo $row["lando"]."-".$row["posxtkodo"]." ".$row["urbo"]."<br>";
         echo "</td>";
      
         echo "</tr>";
         echo "<tr";
-        if ($i%2==1) { echo " bgcolor=\"#d0d8df\"";}
+        if ($nbResultat%2==1) { echo " bgcolor=\"#d0d8df\"";}
         echo "><td colspan=\"2\">";
-        if ($personoj[$i]->get_kialo()!="") { echo $personoj[$i]->get_kialo()."<br>"; }
+        if ($row["kialo"]!="") { echo $row["kialo"]."<br>"; }
   		echo "</td></tr>";
 	}
+
+	if ($nbResultat==0) {
+		echo "<tr><td>";
+		echo "<h1>Aucun élève</h1>";
+		echo "</tr></td>";
+	}
+
 	echo "</table>";
 }
 include "adminkapo.inc.php";
 ?>
 		<div id="adminejo">
 			<ul id="tabnav">
-				<li><a href="administri.php?kategorio=P"><?=$lgv_atendasKorektanton ?></a></li>
-				<li><a href="administri.php?kategorio=SO"><?=$lgv_jamRicevisKorektanton ?></a></li>
-				<li><a href="administri.php?kategorio=S"><?=$lgv_aliajLernantoj ?></a></li>
-				<li><a href="administri.php?kategorio=H"><?=$lgv_haltis ?></a></li>
-				<li><a href="administri.php?kategorio=F"><?=$lgv_finis ?></a></li>
-				<li><a href="administri.php?kategorio=K"><?=$lgv_korektantoj?></a></li>
-				<li><a href="administri.php?kategorio=A"><?=$lgv_administrantoj?></a></li>
-				<li><a href="administri.php?kategorio=I"><?=$lgv_informantoj?></a></li>
+				<li><a href="administri.php?kategorio=P">Attendent un correcteur</a></li>
+				<li><a href="administri.php?kategorio=SO">Ont reçu un correcteur</a></li>
+				<li><a href="administri.php?kategorio=S">Autres élèves</a></li>
+				<li><a href="administri.php?kategorio=H">Ont abandonné</a></li>
+				<li><a href="administri.php?kategorio=F">Ont fini le cours</a></li>
+				<li><a href="administri.php?kategorio=K">Correcteurs</a></li>
+				<li><a href="administri.php?kategorio=A">Administrateurs</a></li>
+				<li><a href="administri.php?kategorio=I">Informateurs</a></li>
 			</ul>
 			<div id="kadro">
-				<? if ($erarkodo=="9") echo "<p class='eraro'><i>$lgv_eraro9</i></p>"; ?>
-				<? if ($erarkodo=="11") echo "<p class='eraro'><i>$lgv_eraro11</i></p>"; ?>
+				<?php if ($erarkodo=="9") echo "<p class='eraro'><i>$lgv_eraro9</i></p>"; ?>
+				<?php if ($erarkodo=="11") echo "<p class='eraro'><i>$lgv_eraro11</i></p>"; ?>
 			
 			<div id="adminG">
 
@@ -105,8 +103,8 @@ include "adminkapo.inc.php";
 			<div id="adminD">
 			<h2>R&eacute;sultat de la recherche :</h2>
 				<div class="encadre">
-	    			<? listiSercxadon($personnomo,$familinomo,$celenirnomo,$retadreso,$urbo,$posxtkodo); ?>
+	    			<?php listiSercxadon($personnomo,$familinomo,$celenirnomo,$retadreso,$urbo,$posxtkodo); ?>
 				</div>
 			</div>
 		</div>
-<? include "pagxpiedo.inc.php"; ?>
+<?php include "pagxpiedo.inc.php"; ?>

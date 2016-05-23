4630e67e2bd72dcf3f4f6ea393cf0362e84e9e63
axel584 <axel584@gmail.com>
2016-05-23 18:38:28 +0200

    ajout de la session pour pouvoir inclure des notes sur les eleves

--- aldoniNoton.php
+++ aldoniNoton.php
@@ -1,6 +1,7 @@
 <?php
 include "lingvo.inc.php";
 require("db.inc.php");
+session_start();
 $teksto=isset($_POST['teksto'])?addslashes($_POST['teksto']):"";
 $stud_id=isset($_POST['stud_id'])?$_POST['stud_id']:0;
 $persono_id=isset($_SESSION['persono_id'])?$_SESSION['persono_id']:0;

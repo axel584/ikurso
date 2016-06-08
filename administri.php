e1182d5b89d894557ba2e5942632ef577f21d297
axel584 <axel584@gmail.com>
2016-06-08 10:13:22 +0200

    Pour l'issue 58, à tester avant de fermer

--- administri.php
+++ administri.php
@@ -624,11 +624,11 @@ include "adminkapo.inc.php";
 										<a href="javascript:document.administri2.submit();">
 										<img src="bildoj/filesaveas.png" border="0"></a>
 									</td>
-									<?php } ?>
 									<td valign="middle">
 										<a href="javascript:document.administri2.action='delete.php';document.administri2.submit();">
 										<img src="bildoj/trashcan_full.png"></a>
 									</td>
+									<?php } ?>
 								</tr>
 							</table>
 							<?php } ?>

bb53483a9392db878d72421237201b6b5e27b38a
Emmanuelle Richard <emmrichard@gmail.com>
2016-06-25 22:27:42 +0200

    ajout hamburger menu et modale connexion dans maquettes

---
+++ maquettes/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+  
+    $(document).ready(function() {
+    Materialize.updateTextFields();
+  });
+      

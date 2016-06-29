f5b3bcde4b3ba924a7321938a2f3f29dcf808a4e
axel584 <axel584@gmail.com>
2016-06-29 22:30:40 +0200

    quelques corrections mineurs

---
+++ ajax/aligxi.php
@@ -0,0 +1,34 @@
+<?php
+include "../util.php";
+$identigilo=isset($_GET['identigilo'])?$_GET['identigilo']:"";
+$pasvorto=isset($_GET['pasvorto'])?$_GET['pasvorto']:"";
+
+$respondo = array();
+
+$query = "select id,aktivigita,pasvorto_md5 from personoj where enirnomo='".$identigilo."'";
+$result = $bdd->query($query);
+if (!$row = $result->fetch()) { // aucune ligne retournée
+	$respondo["mesagxo"]="Identifiant introuvable, cliquez sur le bouton <Créer un compte>";
+	$respondo["type"]="identigilo";
+}
+else {
+	if ($row["aktivigita"]==0) { 
+		$respondo["mesagxo"] = "Ce compte n'est pas validé, merci de cliquer sur le lien reçu par email.";
+		$respondo["type"]="identigilo";
+	} else {
+		if (md5($pasvorto)!=$row["pasvorto_md5"]) {
+			$respondo["mesagxo"] = "Mot de passe incorrect";
+			$respondo["type"]="pasvorto";
+		} else {
+			$respondo["mesagxo"] = "ok";
+			// on memorise l'id en session :
+			$_SESSION["persono_id"]=$row["id"];
+			// trouver l'url où l'on doit atterir
+			$respondo["url"]="administri.php";
+		}
+	}
+
+}
+
+echo json_encode($respondo);
+?>
--- js/ikurso.js
+++ js/ikurso.js
@@ -1,20 +1,19 @@
   $(document).ready(function(){
-  	
   	$( "#connection_button" ).click(function() {
   		$.ajax({
        		url : 'ajax/eniri.php',
        		type : 'GET',
        		dataType : 'json',
-       		data : 'identigilo='+$( "#identigilo" ).val()+"&pasvorto="+$( "#pasvorto" ).val(),
+       		data : 'identigilo='+$( "#eniri_identigilo" ).val()+"&pasvorto="+$( "#eniri_pasvorto" ).val(),
        		success : function(reponse, statut){ 
            		if (reponse.mesagxo!="ok") {
            			if (reponse.type=="identigilo") {
-           				$("label[for='identigilo']").attr('data-error',reponse.mesagxo);
-           				$("#identigilo").addClass("invalid");
+           				$("label[for='eniri_identigilo']").attr('data-error',reponse.mesagxo);
+           				$("#eniri_identigilo").addClass("invalid");
            			}
            			if (reponse.type=="pasvorto") {
-           				$("label[for='pasvorto']").attr('data-error',reponse.mesagxo);
-           				$("#pasvorto").addClass("invalid");	
+           				$("label[for='eniri_pasvorto']").attr('data-error',reponse.mesagxo);
+           				$("#eniri_pasvorto").addClass("invalid");	
            			}
            			return false;
            		} else {
@@ -26,4 +25,33 @@
        		}
     	});
 	});
+
+  	$( "#inscription_button" ).click(function() {
+  		$.ajax({
+       		url : 'ajax/aligxi.php',
+       		type : 'GET',
+       		dataType : 'json',
+       		data : 'identigilo='+$( "#aligxi_identigilo" ).val()+"&pasvorto="+$( "#aligxi_pasvorto" ).val()+"&retadreso="+$("#aligxi_retadreso").val(),
+       		success : function(reponse, statut){ 
+           		if (reponse.mesagxo!="ok") {
+           			if (reponse.type=="identigilo") {
+           				$("label[for='aligxi_identigilo']").attr('data-error',reponse.mesagxo);
+           				$("#aligxi_identigilo").addClass("invalid");
+           			}
+           			if (reponse.type=="aligxi_pasvorto") {
+           				$("label[for='aligxi_pasvorto']").attr('data-error',reponse.mesagxo);
+           				$("#aligxi_pasvorto").addClass("invalid");	
+           			}
+           			return false;
+           		} else {
+           			// On affiche un message qui indique qu'il faut valider le mail
+           			$('#aligxi').closeModal();
+           		}
+       		},
+       		error : function() {
+       			alert("Erreur de connection, contactez les administrateurs");
+       		}
+    	});
+	});
+
   });

b2730c85f561436be2d61749a1621b9ae9cbfa5c
Emmanuelle Richard <emmrichard@gmail.com>
2016-07-02 20:17:55 +0200

    nouveau design pour personinformoj et sxangxipersoninformojn

--- js/init.js
+++ js/init.js
@@ -9,25 +9,30 @@
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-  });
-  
-    $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
 
-  $(document).ready(function(){
+    Materialize.updateTextFields();
+
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-  });
-  
-    $(document).ready(function() {
-	$('.slider').slider({full_width: true});
-});
 
-  $(document).ready(function() {
+	$('.slider').slider({full_width: true});
+
     $('select').material_select();
-  });
-  
-    $(document).ready(function(){
+
     $('.scrollspy').scrollSpy();
-  });
+    $('select').material_select();
+      
+  	$( '.datepicker' ).pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 200, // Creates a dropdown of 100 years to control year
+	    formatSubmit: 'dd/mm/yyyy',
+	    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+	    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+	    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+	    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+	    today: '',
+	    clear: 'effacer',
+	    close: 'fermer'
+	})
+
+});

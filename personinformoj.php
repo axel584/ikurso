d3d5121f7f738d4a8418d44676541e24e311424f
Emmanuelle Richard <emmrichard@gmail.com>
2016-07-02 20:17:55 +0200

    nouveau design pour personinformoj et sxangxipersoninformojn

--- personinformoj.php
+++ personinformoj.php
@@ -1,138 +1,106 @@
 <?php
 include "util.php";
 $pagxtitolo="Données personnelles";
+$korpo="informoj";
 $persono_id=$_SESSION["persono_id"];
 if ($persono_id=="") {header("Location:index.php?erarkodo=8");}
 $persono = apartigiPersonon($persono_id);
-$parto=isset($_GET["parto"])?$_GET["parto"]:"";
-$erarkodo = isset($_GET['erarkodo']) ? $_GET['erarkodo'] : "";
-if ($parto=="") {$parto=1;}
-
-// ***** ER 19/04/2016 : suppression de l'onglet "mes voisins" *****
-$parto=1;
-
 include "pagxkapo.inc.php";
-
-if ($erarkodo=="16") echo "<div class='atentigo'><p class='eraro'>Cours inconnu.</p></div>";
-if ($erarkodo=="17") echo "<div class='atentigo'><p class='eraro'>Attestation introuvable pour ce cours.</p></div>";
 ?>
-		<div id="enhavo">
-		<ul id="tabnav">
-			<!--
-			// ***** ER 19/04/2016 : suppression de l'onglet "mes voisins" *****
-			<li <? if ($parto=="1") {echo " class='aktiva'";}?>><a href="personinformoj.php?parto=1">Donn&eacute;es personnelles</a></li>
-			<li <? if ($parto=="2") {echo " class='aktiva'";}?>><a href="personinformoj.php?parto=2">Mes voisins</a></li>
-			-->
-			<li class="aktiva"><a href="personinformoj.php?parto=1">Données personnelles</a></li>
-		</ul>
-		<div id="kadro">
-		
-		<?php // ***** ER 19/04/2016 ***** if ($parto=="1") { */ ?>
-			<table class="perso">
-			<tbody>
-				<tr>
-					<td class="col1">Nom :</td>
-					<td nowrap><?=$persono['familinomo'];?></td>
-				</tr>
-				<tr>
-					<td class="col1">Prénom :</td>
-					<td nowrap><?=$persono['personnomo'];?>
+
+<main class="row">
+	<article class="col s12 m9 l6 offset-m3 offset-l2">
+		<h1>Données personnelles</h1>
+			
+		<div id="novaUzanto" class="card-panel blue lighten-5 center hide">
+			<p>Votre compte est maintenant activé. Vous pouvez compléter vos informations personnelles.</p>
+		</div>
+		<section id="enhavo">
+			<div class="row">
+				<span class="col s4 label">Nom :</span>
+				<span class="col s6"><?php echo $persono['familinomo'];?></span>
+			</div>
+			<div class="row">
+				<span class="col s4 label">Prénom :</span>
+				<span class="col s6"><?php echo $persono['personnomo'];?>
 					<?php if ($persono['sekso']=='M'){echo " (H)";}
 						elseif ($persono['sekso']=='I'){echo " (F)";}
-					?>
-					</td>
-				</tr>
-				<tr>
-					<td class="col1">Adresse :</td>
-					<td nowrap><?=$persono['adreso1'];?></td>
-				</tr>
-				<?php if ($persono['adreso2']!=""){?>
-				<tr>
-					<td nowrap>&nbsp;</td>
-					<td nowrap><?=$persono['adreso2'];?></td>
-				</tr>
-				<?php }?>
-				<tr>
-					<td>&nbsp;</td>
-					<td><?php echo $persono['posxtkodo']." ".$persono['urbo'];?></td>
-				</tr>
-				<tr>
-					<td nowrap>&nbsp;</td>
-					<td nowrap><?=simplaVorto("nomo","landoj","where kodo='".$persono['lando']."'");?></td>
-				</tr>
-				<tr>
-					<td class="col1">Adresse électronique :</td>
-					<td nowrap><?=$persono['retadreso'];?></td>
-				</tr>
-				<tr>
-					<td  class="col1">Date de naissance :</td>
-					<td>
-						<?php 
-						$nskdt = explode("-",$persono['naskigxdato']);
-						if (count($nskdt)==3) { // si on n'arrive pas à découper la date, c'est qu'elle est vide
+					?></span>
+			</div>
+			
+			<div class="row">
+				<span class="col s4 label">Adresse :</span>
+				<span class="col s6">
+					<?php echo $persono['adreso1']; 
+					if ($persono['adreso2']!="") {
+						echo "<br>".$persono['adreso2'];
+						echo "<br>".$persono['posxtkodo']." ".$persono['urbo'];
+						echo "<br>".simplaVorto("nomo","landoj","where kodo='".$persono['lando']."'");
+					} ?>
+				</span>
+			</div>
+	
+			<div class="row">
+				<span class="col s4 label">E-mail :</span>
+				<span class="col s6"><?php echo $persono['retadreso'];?></span>
+			</div>
+	
+			<div class="row">
+				<span class="col s4 label">Date de naissance :</span>
+				<span class="col s6">
+					<?php $nskdt = explode("-",$persono['naskigxdato']);
+						if (count($nskdt)==3) {
 							if (($nskdt[0]!="00")&&($nskdt[2]!="0000")) {
 								echo $nskdt[2]." ";
 								simplaVorto("nomo","monatoj"," where kodo='".$nskdt[1]."'");
 								echo " ".$nskdt[0];
 							} 
 						}
-						?>
-					</td>
-				</tr>
-				<tr>
-				<?php if ($persono['rajtoj']=="S" || $persono['rajtoj']=="P") {
-				?>	
-					<td class="col1">Dernier cours choisi :</td>
-					<td><?php
-						simplaVorto("nomo","kursoj"," where kodo='".$persono['kurso']."'");
-						?>
-					</td>
-				<?php }else {?>
-					<td class="col1">Droits :</td>
-					<td><?=simplaVorto("nomo","rajtoj"," where kodo='".$persono['rajtoj']."'");;?></td>
-				<?php }?>
-				</tr>
-				<?php if ($persono['rajtoj']=="S") { ?>
-				<tr>
-					<td class="col1">Mon correcteur :</td>
-					
-					<?php 
-					// on récupère les coordonnées du correcteur de l'élève
-					$korektanto = getKorektantonElLernanto($persono['id']);
 					?>
-					<td><?=$korektanto['personnomo'];?> <?=$korektanto['familinomo'];?> : <a href="mailto:<?=$korektanto['retadreso'];?>"><?=$korektanto['retadreso'];?></a></td>
-				</tr>					
-				<?php }?>
-				<tr>
-					<td class="col1" valign="top">Commentaire :</td>
-					<td valign="top"><?php echo stripSlashes(nl2br($persono['kialo']));?></td>
-				</tr>
-				<tr>
-					<td class="col1" valign="top">Cours suivis :</td>
-					<td valign="top">
-						<?php
-						getCoursElLernanto($persono['id']);
-						?>
-					</td>
-				</tr>
-			</tbody>
-			</table>
+				</span>
+			</div>
+			
+			<div class="row">
+				<div class="col s10">
+					<a class="btn waves-effect waves-light blue right" href="sxangxipersoninformojn.php">Modifier</a>
+				</div>
+		</section>
+		
+		<section id="rimarko" class="rimarko primaire-texte texte-moyen">
+			<div class="row">
+				<div class="col s12">
+					<p class="noto">
+					Les informations vous concernant sont destinées à Espéranto-Jeunes et Espéranto-France. 
+					Nous pouvons être amenés à les transmettre à des tiers (association espérantophone régionale ou thématique, autres membres d’Espéranto-France ou Espéranto-Jeunes) ou utiliser ces coordonnées pour vous envoyer des informations concernant l’association par Internet.
+					</p>
+					<p  class="noto">Vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données 
+					qui vous concernent (art. 34 de la loi «Informatique et Libertés») sur la page suivante.
+					</p>
+				</div>
+			</div>
+		</section>
+	</article>
+	
+	<aside class="col s12 m10 l3 offset-m1 offset-l1">
+		<!-- pour les élèves uniquement -->
+		<?php if ($persono['rajtoj']=="S" || $persono['rajtoj']=="P") { ?>
+			<h2>Mes cours</h2>
+			
+			<p>Dernier cours choisi : <?php simplaVorto("nomo","kursoj"," where kodo='".$persono['kurso']."'"); ?></p>
+			<p>Mon correcteur est 
+				<?php $korektanto = getKorektantonElLernanto($persono['id']); echo $korektanto['personnomo']." ".$korektanto['familinomo']." ("; ?>
+				<a href="mailto:<?php echo $korektanto['retadreso'];?>"><?php echo $korektanto['retadreso'];?></a></p>
+			<p>Cours suivi : <?php getCoursElLernanto($persono['id']);?>			
+		<?php } else { ?>
+		<!-- pour correcteurs et les admin -->
+			<h2>Mes élèves</h2>
+			<p>Droits : <?=simplaVorto("nomo","rajtoj"," where kodo='".$persono['rajtoj']."'");;?></p>
+		<?php } ?>
+	</aside>
 
-				<p class="rimarko">
-				Les informations vous concernant sont destinées à Espéranto-Jeunes et Espéranto-France. 
-				Nous pouvons être amenés à les transmettre à des tiers (association espérantophone régionale 
-				ou thématique, autres membres d’Espéranto-France ou Espéranto-Jeunes) ou utiliser ces coordonnées pour vous envoyer 
-				des informations concernant l’association par Internet.
-				</p>
-				<p  class="rimarko">Vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données 
-				qui vous concernent (art. 34 de la loi «Informatique et Libertés») sur la page suivante.
-				</p>
-				<br /><p>
-				<!--
-				<?=$lgv_porsxangxi?> : <input class="bouton" type="button" name="sxangxi" value="<?=$lgv_sxangxu;?>" onClick="document.location.href='sxangxipersoninformojn.php';">
-				-->
-				<a class="bouton" href="sxangxipersoninformojn.php">Modifier mes données personnelles</a>
-				</p>
-				
-   	</div>
-<? include "pagxpiedo.inc.php";?>                     
+	</article>
+	
+	
+</main>
+			
+<?php include "pagxpiedo.inc.php";?>
--- sxangxipersoninformojn.php
+++ sxangxipersoninformojn.php
@@ -1,100 +1,121 @@
 <?php
 include "util.php";
-$pagxtitolo="Modification de vos données personnelles";
+$pagxtitolo="Modification des données personnelles";
+$korpo="informoj";
 $persono_id=$_SESSION["persono_id"];
 if ($persono_id=="") {header("Location:index.php?erarkodo=8");}
 $persono = apartigiPersonon($persono_id);
-$parto="1";
 $erarkodo = isset($_GET['erarkodo']) ? $_GET['erarkodo'] : "";
 include "pagxkapo.inc.php";
 ?>
-<div id="enhavo">
-		<ul id="tabnav">
-			<li class='aktiva'><a href="personinformoj.php?parto=1">Donn&eacute;es personnelles</a></li>
-		</ul>
-		<?php if ($erarkodo=="2") echo "<div class='atentigo'><p class='eraro'>Vous n’avez pas rempli toutes les données obligatoires du formulaire d’inscription.</p></div>"; ?>
-		<?php if ($erarkodo=="3") echo "<div class='atentigo'><p class='eraro'>Vous avez tapé deux mots de passe différents.</p></div>"; ?>
-		<?php if ($erarkodo=="6") echo "<div class='atentigo'><p class='eraro'>Adresse électronique invalide.</p></div>"; ?>
-		<?php if ($erarkodo=="7") echo "<div class='atentigo'><p class='eraro'>Cet identifiant existe déjà. Veuillez en choisir un autre, merci.</p></div>"; ?>
-		<div id="kadro">
+
+<main class="row">
+	<article class="col s12 m10 l7 offset-m2 offset-l2">
+		<h1>Données personnelles</h1>
+
+		<div id="novaUzanto" class="card-panel blue lighten-5 center hide">
+			<p>Votre compte est maintenant activé. Vous pouvez compléter vos informations personnelles.</p>
+		</div>
+
+		<p>Votre identifiant est : <?php echo $persono['enirnomo'];?><br>
+			<span class="noto contraste-texte-A">(Pour changer votre adresse électronique, veuillez contacter les administrateurs du cours.)</span>
+		</p>
+		
 		<form name="inscription" action="sxangxipersoninformojn2.php" method="POST">
-		<?php if (isset($rapidvojo)) { echo "<input type=\"hidden\" name=\"rapidvojo\" value=\"$rapidvojo\">";} ?>
-		<input type="hidden" name="kurso" value="<?=$persono['kurso'];?>">
-		<table class="perso">
-		<tbody>
-			<tr>
-				<td class="col1">Nom :</td>
-				<td><input type="text" name="familinomo" size="30" value="<?=$persono['familinomo'];?>"></td>
-			</tr>
-			<tr>
-				<td class="col1">Prénom :</td>
-				<td>
-					<input type="text" name="personnomo" size="30" value="<?=$persono['personnomo'];?>">
-					H. <input type="radio" name="sekso" value="M" <?php if ($persono['sekso']=="M"){echo "checked";}?>>
-					F. <input type="radio" name="sekso" value="I" <?php if ($persono['sekso']=="I"){echo "checked";}?>>
-				</td>
-			</tr>
-			<tr>
-				<td class="col1">Adresse :</td>
-				<td><input type="text" name="adreso1"  size="30" value="<?php echo stripslashes($persono['adreso1']);?>"></td>
-			</tr>
-			<tr>
-				<td  class="col1">Adresse (suite) :</td>
-				<td><input type="text" name="adreso2" size="30" value="<?php echo stripslashes($persono['adreso2']);?>"></td>
-			</tr>
-			<tr>
-				<td class="col1">Code Postal :</td>
-				<td><input type="text" name="posxtkodo" value="<?=$persono['posxtkodo'];?>"></td>
-			</tr>
-			<tr>
-				<td class="col1">Ville :</td>
-				<td><input type="text" name="urbo" size="30" value="<?=$persono['urbo'];?>"></td>
-			</tr>
-			<tr>
-				<td class="col1">Pays :</td>
-				<td>
-					<?php 
-					konstruiMenuon("lando","landoj","kodo","nomo",$persono['lando']," order by nomo","-- pays --"); 
-					?>
-				</td>
-			<tr>
-				<td class="col1">Adresse électronique :</td>
-				<td><?=$persono['retadreso'];?></td>
-			</tr>
-			<tr>
-				<td colspan="2" class="col1 rimarko">(Pour changer votre adresse électronique, veuillez 
-				<a href="reago.php">contacter les administrateurs du cours</a>.)</td>
-			</tr>
-			<tr>
-				<td class="col1">Date de naissance :</td>
-				<td>
-					<?php 
-					//ereg("([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})",$persono['naskigxdato'],$nskdt);
-					$nskdt=explode("-",$persono['naskigxdato']);
-					if (count($nskdt)!=3) {
-						$nskdt=array("","","");
-					}
-					?>
-					<input type="text" name="naskigxdato_tago" size="3" maxlength="2" value="<?=$nskdt[2];?>">
-					<?php konstruiMenuon("naskigxdato_monato","monatoj","kodo","nomo",$nskdt[1],"","-- mois --");?>
-					<input type="text" name="naskigxdato_jaro" size="5"  maxlength="4" value="<?=$nskdt[0];?>">
-				</td>
-			</tr>
-		</tbody>
-		</table>
-		<p>Autres précisions : <i>Qu’attendez-vous du cours, où avez-vous appris son existence,...</i></p>
-		<p>
-			<textarea name="kialo" cols="70" rows="5"><?=stripslashes($persono['kialo']);?></textarea>
-		</p>
-		<p class="rimarko">
-		Les informations vous concernant sont destinées à Espéranto-Jeunes et Espéranto-France. 
-		Nous pouvons être amenés à les transmettre à des tiers (association espérantophone régionale 
-		ou thématique, autres membres d’Espéranto-France ou Espéranto-Jeunes) ou utiliser ces coordonnées pour vous envoyer 
-		des informations concernant l’association par Internet. Si vous ne le souhaitez pas, veuillez cocher cette case :
-		<input type="checkbox" name="stopInfo" <?php if ($persono['stop_info']=="J") {echo "checked";}?>>.
-		</p>
-		<p>
-			<input class="bouton" type="submit" name="Submit" value="Envoyer">
-		</p>		
-	</div>
+			
+			<div class="row">
+				<div class="input-field col s6 l4">
+					<input id="personnomo" name="personnomo" value="<?php echo $persono['personnomo'];?>" type="text" class="validate" required="" aria-required="true">
+					<label for="personnomo">Prénom</label>
+				</div>
+
+				<div class="input-field col s6 l4">
+					<input id="familinomo" name="familinomo" value="<?php echo $persono['familinomo'];?>" type="text" class="validate" required="" aria-required="true">
+					<label for="familinomo">Nom</label>
+				</div>
+			</div>
+
+			<div class="row">
+
+				<div class="col s6 m4">
+					<label for="naskigxdato" class="">Date de naissance</label>
+					<input id="naskigxdato" type="date" class="datepicker"s>
+				</div>
+				<div class="col s3">
+					<input type="radio" name="sekso" id="seksoM" value="M" <?php if ($persono['sekso']=="M"){echo "checked";}?>>
+					<label for="seksoM">H.</label>
+					<input type="radio" name="sekso" id="seksoI" value="I" <?php if ($persono['sekso']=="I"){echo "checked";}?>>
+					<label for="seksoI">F.</label>
+				</div>
+			</div>
+
+			<div class="row">
+				<span class="input-field col s12">
+					<input type="text" name="adreso1" value="<?php echo stripslashes($persono['adreso1']);?>" />
+					<label for="adreso1">Adresse</label>
+				</span>
+				<span class="input-field col s12">
+					<input type="text" name="adreso2" value="<?php echo stripslashes($persono['adreso2']);?>" />
+					<label for="adreso2">Adresse (suite)</label>
+				</span>
+			</div>
+
+			<div class="row">
+				<div class="input-field col s4 m2">
+					<input type="text" name="posxtkodo" value="<?php echo $persono['posxtkodo'];?>" />
+					<label for="posxtkodo">Code Postal</label>
+				</div>
+				
+				<div class="input-field col s8 m5">
+					<input type="text" name="urbo" value="<?php echo $persono['urbo'];?>" />
+					<label for="urbo">Ville</label>
+				</div>
+
+				<div class="input-field col s12 m5">
+					<select name="lando" id="lando">
+					<!-- à remplacer par des données dynamiques -->
+						<option value="DE">Allemagne</option>
+						<option value="AD">Andorre</option>
+						<option value="AO">Angola</option>
+						<option value="FR">France</option>
+						<option value="GA">Gabon</option>
+						<option value="GH">Ghana</option>
+						<option value="GB">Grande Bretagne</option>
+						<option value="CZ">République Tchèque</option>
+						<option value="CH">Suisse</option>
+						<option value="TN">Tunisie</option>
+						<option value="XX">__Autre__</option>						
+					</select>
+					<label for="lando">Pays</label>
+				</div>
+			</div>
+			
+			<section id="rimarko" class="rimarko primaire-texte texte-moyen">
+				<div class="row">
+					<div class="col s12">
+						<p class="noto">
+						Les informations vous concernant sont destinées à Espéranto-Jeunes et Espéranto-France. 
+						Nous pouvons être amenés à les transmettre à des tiers (association espérantophone régionale ou thématique, autres membres d’Espéranto-France ou Espéranto-Jeunes) ou utiliser ces coordonnées pour vous envoyer des informations concernant l’association par Internet.<br>
+						<input type="checkbox" id="stopInfo" name="stopInfo" />
+						<label for="stopInfo">Je ne souhaite pas que mes données personnelles soit transmises à tiers, ni utilisées pour m’envoyer des informations sur l’association.</label>
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<div class="row">
+				<div class="col s12">
+					<input class="btn blue right" type="submit" name="Submit" value="Enregister les modifications">
+				</div>
+			</div>	
+		</form>
+
+		<div class="rimarko">
+			<p  class="noto">Vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données 
+			qui vous concernent (art. 34 de la loi «Informatique et Libertés»).
+			</p>
+		</div>
+
+	</article>
+</main>
 <?php include "pagxpiedo.inc.php";?>            

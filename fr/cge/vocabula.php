a409ceac472e72125861a73e13ee78bb1ff6d545
Emmanuelle Richard <emmrichard@gmail.com>
2016-07-18 23:06:12 +0200

    page vocabulaire dledk

--- fr/cge/vocabula.php
+++ fr/cge/vocabula.php
@@ -6,7 +6,19 @@ $pagxtitolo="Lexique";
 $metodo=isset($_GET["metodo"])?$_GET["metodo"]:"U";
 include "dlekkapo.inc.php";
 ?>
-		<div class="klarigo">
-<?php include "vortlisto.inc.php"; ?>
-		</div>
+<div class="row">
+	<article class="col s12 m9 offset-m1">
+		<section id="leciono-enhavo">
+		<h2>Lexique</h2>
+	<?php include "vortlisto.inc.php"; ?>
+	</article>
+	
+	<aside class="col s12 m2">
+		<p>Téléchargez ce lexique au format PDF&nbsp;:<br> 
+		<a href="http://ikurso.esperanto-jeunes.org/doc/LexiqueDLEK-Ikurso.pdf">
+		<img src="http://ikurso.esperanto-jeunes.org/bildoj/pdf-dist.png" alt="Lexique - 55 Ko" 
+		title="Lexique - 55 Ko" align="bottom" border="0"></a></p>
+	</aside>
+
+</div>
 <?php include "dlekpiedo.inc.php"; ?>

e0bc3a167c1368ee624243a2bbd3d8ae56b4eb24
Emmanuelle Richard <emmrichard@gmail.com>
2016-07-31 23:40:29 +0200

    index Gerda et Lasu

--- fr/gerda/lasu00.php
+++ fr/gerda/lasu00.php
@@ -1,84 +1,112 @@
-<?php
-$titolo="Préambule";
-$gxisdatigDato="2015-06-14";
-include "gerdakapo.inc.php";
+<?php 
+include "gerdakapo.inc.php"; 
 ?>
-<div class="klarigo">
-	
-	<h3><i>Lasu min paroli plu !</i> : un recueil de textes de lecture</h3>
-	
-	<p>Claude Piron a réalisé un recueil de courtes histoires spécialement
-	adapté pour les étudiants du cours 
-	<a href="index.php"><i>Gerda malaperis</i></a>. Nous remercions
-	chaleureusement Claude Piron pour son aimable permission de reproduire
-	ce texte ici-même.</p>
+	<div class="row">
+		<article class="col s12 m10 l7 offset-m1 offset-l1">
+			<section id="leciono-kapo">
+				<h1>Cours Gerda malaperis</h1>
+			</section>
+			
+			<section id="leciono-enhavo">
+				<h2><i>Lasu min paroli plu !</i> : un recueil de textes de lecture</h2>
+			
+				<p>Claude Piron a réalisé un recueil de courtes histoires spécialement
+				adapté pour les étudiants du cours 
+				<a href="<?php echo $vojo;?>fr/gerda/index.php"><i>Gerda malaperis</i></a>. Nous remercions
+				chaleureusement Claude Piron pour son aimable permission de reproduire
+				ce texte ici-même.</p>
+				
+				<div class="row">
+					<div class="col s12">
+						<div class="card-panel blue lighten-5">
+							Vous trouverez chaque chapitre de <i>Lasu min paroli plu !</i> dans le chapitre correspondant du cours <i>Gerda malaperis</i>
+						</div>
+					</div>
+				</div>
+		
+				<h3 id="avant-propos">Avant-propos</h3>
+			
+				<p>On ne connaît pas vraiment l’espéranto si l’on n’est pas capable de tirer parti des potentialités présentes dans les bases minimales du vocabulaire et de la grammaire. L’élève apprend un nombre très restreint de nouvelles racines dans chaque chapitre de <i>Gerda malaperis!</i> mais leur potentiel expressif est immense. C’est donc l’un des buts de ce recueil que de l’aider à découvrir cette richesse.</p>
+				<p>Pour atteindre ce but il est conseillé à l’élève qui vient d’étudier un chapitre de <i>Gerda malaperis!</i> de lire immédiatement le texte de même numéro de ce recueil. Il connaîtra tout ce qui est nécessaire, au niveau grammatical et lexical, pour comprendre ce texte. Par exemple, s’il a appris la matière des chapitres 1 à 7 inclus, il ne rencontrera dans le texte numéro 7 rien qu’il n’ait déjà étudié antérieurement. Ou plutôt il n’y rencontrera aucune nouveauté grammaticale ni aucune racine nouvelle mais il sera confronté à des mots nouveaux. La nécessité d’en deviner le sens, sur la base des éléments déjà acquis, l’initiera progressivement au système de dérivation et de 
+				composition de l’espéranto.</p>
+				<p>Affermir les connaissances acquises, tel est le second but de ce recueil. La totalité du matériau linguistique de <i>Gerda malaperis!</i> y est réutilisée et les racines qui n’apparaissent pas assez souvent dans le roman y sont répétées suffisamment pour qu’elles se fixent dans la mémoire de l’élève avec un minimum d’effort de sa part. L’effet de fixation se trouvera ici renforcé par le fait que les éléments en question sont employés dans des textes dont l’atmosphère et les thèmes sont tout autres que dans le roman.</p>
+				<p>Ce recueil se compose pour l’essentiel de monologues. L’élève peut ainsi s’exercer seul : en lisant le texte à haute voix il utilisera une langue parlée authentique et il pourra s’identifier au personnage tout comme le ferait un acteur. Ce type d’identification est un des ressorts psychologiques connus pour favoriser l’assimilation d’une langue.</p>
+				<p>Entre les numéros 13 et 24, le recueil présente deux textes en parallèle à chaque chapitre de <i>Gerda malaperis!</i>. L’élève aura loisir de choisir le morceau qui lui plaît le mieux au premier coup d’œil, à moins qu’il ne veuille lire les deux.</p>
+				<p>Si, grâce à ces exemples d’espéranto parlé, rédigés avec un vocabulaire limité, l’élève arrive à prendre conscience des possibilités multiples et diverses que lui offrent ces éléments minimaux de la langue pour s’exprimer, alors le but principal de cet ouvrage aura été atteint.</p>
+		
+				<p>Claude Piron (traduction MB)</p>
+			
+				<h3 id="antauxparolo">Préambule</h3>
+			
+				<p>Oni ne vere posedas Esperanton, se oni ne kapablasekspluati la multajn eblecojn, kiujn jam entenas minimuma vorta kaj gramatika scio. Nur malmultajn radikojn la lernanto ekkonas en ĉiu unuopa ĉapitro de <i>Gerda malaperis !</i>, sed ilia latenta esprimpovo 	estas grandega. Helpi lin malkovri tiun riĉecon estas unu el la celoj de ĉi tiu kolekto.</p>
+				<p>Por tion atingi, la lernanto saĝe agos, se, studinte 	ĉapitron de <i>Gerda malaperis !</i>, li tuj legos la samnumeran tekston en ĉi tiu kolekto. Li jam konos ĉion necesan, vorte kaj gramatike, por kompreni ĝin. Ekzemple, se li lernis la materialon de <i>Gerda malaperis !</i> ĝis, inkluzive, ĉapitro 7, li trovos sub la numero 7 de ĉi tiu kolekto tekston, en kiu nenio nelernita prezentiĝos. Aŭ, pli ĝuste, li renkontos en tiu teksto neniun novan gramatikaĵon kaj neniun novan <b>radikon</b>, sed jes novajn <b>vortojn</b>, kaj la neceso diveni ilian sencon surbaze de la elementoj, kiujn li lernis, iom post iom enkondukos lin en la vortfaran sistemon de Esperanto.</p>
+				<p>Dua celo de ĉi tiu kolekto estas firmigi la 	akiritajn sciojn. La tuta lingva materialo de <i>Gerda malaperis !</i>, estas ĉi tie reuzata, kaj tiuj radikoj, kiuj aperis tro malofte en la romaneto, estas daŭre ripetataj en ĉi tiuj tekstoj, tiel ke ili firme fiksiĝos en la memoro de la lernanto kun peno minimuma. Al la firmiga efiko kontribuos ankaŭ la fakto, ke la koncernaj elementoj 	troviĝas ĉi tie en tekstoj kun tute alia atmosfero, pritraktantaj tute aliajn temojn.</p>
+				<p>Ĉi tiu kolekto konsistas plejparte el monologoj. Tio 	ebligas al la lernanto ekzerci sin sola: legante tekston laŭtvoĉe, 	li uzos vere parolan lingvon, kaj povos identigi sin al la koncerna rolulo, kiel aktoro faras. Tia identigo estas unu el la konataj 	psikologiaj rimedoj favori la asimiladon de lingvo.</p>
+				<p>De numero 13 ĝis numero 24, inkluzive, du malsamaj tekstoj prezentiĝas por ĉiu paralela ĉapitro de <i>Gerda malaperis !</i>. La lernanto tiel povos elekti la pecon, kiu unuarigarde pli plaĉas al li, se li ne volos legi ambaŭ.</p>
+				<p>Se, dank' al ĉi tiuj ekzemploj de parola Esperanto, verkitaj kun malgranda vortprovizo, la lernanto konsciiĝos, kiel multe kaj diverse minimumaj lingvo-elementoj ebligas esprimi sin, la 	ĉefa celo de ĉi tiu verko estos atingita.</p>
+				<p>Claude Piron</p>
+				
+			</section>
+		</article>
+				
+		<aside class="col s12 m10 l3 offset-m1 push-l1">
+			<ul class="collapsible" data-collapsible="expandable">
+				<li>
+					<div class="collapsible-header active"><i class="material-icons">toc</i>Sommaire</div>
+					<div class="collapsible-body">
+						<ul id="enhavtabelo" class="collection">
+							<li><a href="#avant-propos">Avant-propos</a></li>
+							<li><a href="#antauxparolo">Antaŭparolo</a></li>
+							<li><a href="#enhavo">Table des matières / Enhavo</a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+			<ul class="collapsible" data-collapsible="expandable">
+				<li>
+					<div class="collapsible-header active"><i class="material-icons">toc</i>Chapitres de <em>Lasu min paroli plu!</em></div>
+					<div class="collapsible-body">
+						<ul id="enhavtabelo" class="collection">
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap01.php?section=5">1. Izabela</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap02.php?section=5">2. Stranga strangulo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap03.php?section=5">3. Amdeklaro</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap04.php?section=5">4. Lingvistiko</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap05.php?section=5">5. Ĉu vere vi ne volas labori?</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap06.php?section=5">6. La ulo, kiu ne plu pensas</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap07.php?section=5">7. Oni venis el alia mondo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap08.php?section=5">8. Iru kiel vi volas</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap09.php?section=5">9. Zorgi pri zorgado</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap10.php?section=5">10. La fortegulo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap11.php?section=5">11. Malkontentas la mekanikisto</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap12.php?section=5">12. Jen vi ree trinkas!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap13.php?section=5">13. Revenis amo, feliĉu ni!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap14.php?section=5">14a. Silentu, enaj voĉoj</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap14b.php?section=5">14b. Timiga diro</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap15.php?section=5">15a. Mistero en papervendejo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap15b.php?section=5">15b. Du raportoj pri vendado</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap16.php?section=5">16a. La vesperfuŝulo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap16b.php?section=5">16b. Ĉu malĝojon memori, aŭ imagi feliĉon ?</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap17.php?section=5">17a. Edzokaptaj inoj</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap17b.php?section=5">17b. Instruista amo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap18.php?section=5">18a. La letersoifa patrino</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap18b.php?section=5">18b. Mi atendas gravan leteron</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap19.php?section=5">19. Dormu trankvile!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap20.php?section=5">20a. La fakulo-instruisto venis de malproksime</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap20b.php?section=5">20b. Troa scivolo kondukas morten</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap21.php?section=5">21a. Ho, bela naskiĝurbo!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap21b.php?section=5">21b. Adiaŭ, lando amata!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap22.php?section=5">22a. Ek al fora insulo!</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap22b.php?section=5">22b. Skribas malliberulo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap23.php?section=5">23a. Sekreta servo</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap23b.php?section=5">23b. Ameriko, jen mi venas</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap24.php?section=5">24a. Literaturo amindas, sed tamen...</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap24b.php?section=5">24b. La ĝojoj de petveturado</a></li>
+							<li><a href="<?php echo $vojo;?>fr/gerda/cxap25.php?section=5">25. La tri plendoj de S-ro Kursano</a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</aside>
+	</div>
 
-	<ul class="enhavo">
-		<li><a href="#avant-propos">Avant-propos</a></li>
-		<li><a href="#antauxparolo">Antaŭparolo</a></li>
-		<li><a href="#enhavo">Table des matières / Enhavo</a></li>
-	</ul>
-
-	<h3 id="avant-propos">Avant-propos</h3>
-
-	<p>On ne connaît pas vraiment l’espéranto si l’on n’est pas capable de tirer parti des potentialités présentes dans les bases minimales du vocabulaire et de la grammaire. L’élève apprend un nombre très restreint de nouvelles racines dans chaque chapitre de <i>Gerda malaperis!</i> mais leur potentiel expressif est immense. C’est donc l’un des buts de ce recueil que de l’aider à découvrir cette richesse.</p>
-	<p>Pour atteindre ce but il est conseillé à l’élève qui vient d’étudier un chapitre de <i>Gerda malaperis!</i> de lire immédiatement le texte de même numéro de ce recueil. Il connaîtra tout ce qui est nécessaire, au niveau grammatical et lexical, pour comprendre ce texte. Par exemple, s’il a appris la matière des chapitres 1 à 7 inclus, il ne rencontrera dans le texte numéro 7 rien qu’il n’ait déjà étudié antérieurement. Ou plutôt il n’y rencontrera aucune nouveauté grammaticale ni aucune racine nouvelle mais il sera confronté à des mots nouveaux. La nécessité d’en deviner le sens, sur la base des éléments déjà acquis, l’initiera progressivement au système de dérivation et de 
-	composition de l’espéranto.</p>
-	<p>Affermir les connaissances acquises, tel est le second but de ce recueil. La totalité du matériau linguistique de <i>Gerda malaperis!</i> y est réutilisée et les racines qui n’apparaissent pas assez souvent dans le roman y sont répétées suffisamment pour qu’elles se fixent dans la mémoire de l’élève avec un minimum d’effort de sa part. L’effet de fixation se trouvera ici renforcé par le fait que les éléments en question sont employés dans des textes dont l’atmosphère et les thèmes sont tout autres que dans le roman.</p>
-	<p>Ce recueil se compose pour l’essentiel de monologues. L’élève peut ainsi s’exercer seul : en lisant le texte à haute voix il utilisera une langue parlée authentique et il pourra s’identifier au personnage tout comme le ferait un acteur. Ce type d’identification est un des ressorts psychologiques connus pour favoriser l’assimilation d’une langue.</p>
-	<p>Entre les numéros 13 et 24, le recueil présente deux textes en parallèle à chaque chapitre de <i>Gerda malaperis!</i>. L’élève aura loisir de choisir le morceau qui lui plaît le mieux au premier coup d’œil, à moins qu’il ne veuille lire les deux.</p>
-	<p>Si, grâce à ces exemples d’espéranto parlé, rédigés avec un vocabulaire limité, l’élève arrive à prendre conscience des possibilités multiples et diverses que lui offrent ces éléments minimaux de la langue pour s’exprimer, alors le but principal de cet ouvrage aura été atteint.</p>
-
-	<p>Claude Piron (traduction MB)</p>
-
-	<h3 id="antauxparolo">Préambule</h3>
-
-	<p>Oni ne vere posedas Esperanton, se oni ne kapablasekspluati la multajn eblecojn, kiujn jam entenas minimuma vorta kaj gramatika scio. Nur malmultajn radikojn la lernanto ekkonas en ĉiu 	unuopa ĉapitro de <i>Gerda malaperis !</i>, sed ilia latenta esprimpovo 	estas grandega. Helpi lin malkovri tiun riĉecon estas unu el la celoj de ĉi tiu kolekto.</p>
-	<p>Por tion atingi, la lernanto saĝe agos, se, studinte 	ĉapitron de <i>Gerda malaperis !</i>, li tuj legos la samnumeran tekston en ĉi tiu kolekto. Li jam konos ĉion necesan, vorte kaj gramatike, por kompreni ĝin. Ekzemple, se li lernis la materialon de <i>Gerda malaperis !</i> ĝis, inkluzive, ĉapitro 7, li trovos sub la numero 7 de ĉi tiu kolekto tekston, en kiu nenio nelernita prezentiĝos. Aŭ, pli ĝuste, li renkontos en tiu teksto neniun novan gramatikaĵon kaj neniun novan <b>radikon</b>, sed jes novajn <b>vortojn</b>, kaj la neceso diveni ilian sencon surbaze de la elementoj, kiujn li lernis, iom post iom enkondukos lin en la vortfaran sistemon de Esperanto.</p>
-	<p>Dua celo de ĉi tiu kolekto estas firmigi la 	akiritajn sciojn. La tuta lingva materialo de <i>Gerda malaperis !</i>, estas ĉi tie reuzata, kaj tiuj radikoj, kiuj aperis tro malofte en la romaneto, estas daŭre ripetataj en ĉi tiuj tekstoj, tiel ke ili firme fiksiĝos en la memoro de la lernanto kun peno minimuma. Al la firmiga efiko kontribuos ankaŭ la fakto, ke la koncernaj elementoj 	troviĝas ĉi tie en tekstoj kun tute alia atmosfero, pritraktantaj 	tute aliajn temojn.</p>
-	<p>Ĉi tiu kolekto konsistas plejparte el monologoj. Tio 	ebligas al la lernanto ekzerci sin sola: legante tekston laŭtvoĉe, 	li uzos vere parolan lingvon, kaj povos identigi sin al la koncerna rolulo, kiel aktoro faras. Tia identigo estas unu el la konataj 	psikologiaj rimedoj favori la asimiladon de lingvo.</p>
-	<p>De numero 13 ĝis numero 24, inkluzive, du malsamaj tekstoj prezentiĝas por ĉiu paralela ĉapitro de <i>Gerda malaperis !</i>. La lernanto tiel povos elekti la pecon, kiu unuarigarde pli plaĉas al li, se li ne volos legi ambaŭ.</p>
-	<p>Se, dank' al ĉi tiuj ekzemploj de parola Esperanto, verkitaj kun malgranda vortprovizo, la lernanto konsciiĝos, kiel multe kaj diverse minimumaj lingvo-elementoj ebligas esprimi sin, la 	ĉefa celo de ĉi tiu verko estos atingita.</p>
-	<p>Claude Piron</p>
-	
-	<h3 id="enhavo">Table des matières / Enhavo</h3>
-
-		<ul>
-			<li><a href='lasu01.php'>1. Izabela</a></li>
-			<li><a href='lasu02.php'>2. Stranga strangulo</a></li>
-			<li><a href='lasu03.php'>3. Amdeklaro</a></li>
-			<li><a href='lasu04.php'>4. Lingvistiko</a></li>
-			<li><a href='lasu05.php'>5. Ĉu vere vi ne volas labori?</a></li>
-			<li><a href='lasu06.php'>6. La ulo, kiu ne plu pensas</a></li>
-			<li><a href='lasu07.php'>7. Oni venis el alia mondo</a></li>
-			<li><a href='lasu08.php'>8. Iru kiel vi volas</a></li>
-			<li><a href='lasu09.php'>9. Zorgi pri zorgado</a></li>
-			<li><a href='lasu10.php'>10. La fortegulo</a></li>
-			<li><a href='lasu11.php'>11. Malkontentas la mekanikisto</a></li>
-			<li><a href='lasu12.php'>12. Jen vi ree trinkas!</a></li>
-			<li><a href='lasu13.php'>13. Revenis amo, feliĉu ni!</a></li>
-			<li><a href='lasu14.php'>14a. Silentu, enaj voĉoj</a></li>
-			<li><a href='lasu14b.php'>14b. Timiga diro</a></li>
-			<li><a href='lasu15.php'>15a. Mistero en papervendejo</a></li>
-			<li><a href='lasu15b.php'>15b. Du raportoj pri vendado</a></li>
-			<li><a href='lasu16.php'>16a. La vesperfuŝulo</a></li>
-			<li><a href='lasu16b.php'>16b. Ĉu malĝojon memori, aŭ imagi feliĉon ?</a></li>
-			<li><a href='lasu17.php'>17a. Edzokaptaj inoj</a></li>
-			<li><a href='lasu17b.php'>17b. Instruista amo</a></li>
-			<li><a href='lasu18.php'>18a. La letersoifa patrino</a></li>
-			<li><a href='lasu18b.php'>18b. Mi atendas gravan leteron</a></li>
-			<li><a href='lasu19.php'>19. Dormu trankvile!</a></li>
-			<li><a href='lasu20.php'>20a. La fakulo-instruisto venis de malproksime</a></li>
-			<li><a href='lasu20b.php'>20b. Troa scivolo kondukas morten</a></li>
-			<li><a href='lasu21.php'>21a. Ho, bela naskiĝurbo!</a></li>
-			<li><a href='lasu21b.php'>21b. Adiaŭ, lando amata!</a></li>
-			<li><a href='lasu22.php'>22a. Ek al fora insulo!</a></li>
-			<li><a href='lasu22b.php'>22b. Skribas malliberulo</a></li>
-			<li><a href='lasu23.php'>23a. Sekreta servo</a></li>
-			<li><a href='lasu23b.php'>23b. Ameriko, jen mi venas</a></li>
-			<li><a href='lasu24.php'>24a. Literaturo amindas, sed tamen...</a></li>
-			<li><a href='lasu24b.php'>24b. La ĝojoj de petveturado</a></li>
-			<li><a href='lasu25.php'>25. La tri plendoj de S-ro Kursano</a></li>
-		</ul>
-</div>
 <?php include "gerdapiedo.inc.php" ?>

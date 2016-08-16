c298d6be69b8c7ac4be6b0cddad870d4ef2464d7
Emmanuelle Richard <emmrichard@gmail.com>
2016-08-17 01:00:35 +0200

    début maquette page Mes élèves

--- js/init.js
+++ js/init.js
@@ -54,4 +54,9 @@
     transition:0
   });
 
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
 });

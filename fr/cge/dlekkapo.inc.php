4597b4d79bb09232a15642738bfe8cddb325eb51
Xavier.Meunier <xavier.meunier@orange.fr>
2016-08-22 09:22:22 +0200

    ajout d'un espace manquant avant champ value dans les balises input des questiosn sans x2u

--- fr/cge/dlekkapo.inc.php
+++ fr/cge/dlekkapo.inc.php
@@ -58,9 +58,9 @@ function ekzerco($x2u, $nbLig, $cols6="N") {
 				echo " READONLY onClick='window.alert(\"Identifiez-vous en haut à droite pour pouvoir remplir les exercices\");'";
 			}
 			if ($x2u) {
-				echo " onkeyup='xAlUtf8(this)' ";
+				echo " onkeyup='xAlUtf8(this)'";
 			}
-			echo "value=\"";
+			echo " value=\"";
 			$var="res_ekz".$numcxap."_".sprintf('%02d', $k);
 			if (isset($memorkurso[$var])){
 				echo htmlspecialchars(stripslashes($memorkurso[$var]));

80d50cb48b9ea8c00ffed01dba1824885855c12e
Emmanuelle Richard <emmrichard@gmail.com>
2016-11-18 15:39:11 +0100

    modif stirpanelo

--- stirpanelo.php
+++ stirpanelo.php
@@ -7,38 +7,24 @@ include "pagxkapo.inc.php";
 ?>
 
 <div class="row">
-	<div class="col s12 m3">
-		<ul class="collapsible">
-			<li>
-				<div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Carnet de bord</div>
-				<div class="collapsible-body">
-					<p><span class="label">Début : </span><input id="debut" name="debut" type="date" value="" class="datepicker" /></p>
-					<p><span class="label">Fin : </span><input id="fin" name="fin" type="date" value="" class="datepicker" /></p>
-					<p><span class="label">Type : </span><input type="text" name="type" value="" /></p>
-					<p><span class="label">Utilisateur : </span><input type='text' name='persono_id' value="" /></p>
-					<a href="" class="btn waves-effect waves-light blue">Afficher</a>
-				</div>
-			</li>
-			<li>
-				<div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Correcteurs</div>
-				<div class="collapsible-body">
-					<p>option</p>
-					<p>etc.</p>
-					<a href="" class="btn waves-effect waves-light blue">Afficher</a>
-				</div>
-			</li>
-			<li>
-				<div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Leçons</div>
-				<div class="collapsible-body">
-					<p>option</p>
-					<p>etc.</p>
-					<a href="" class="btn waves-effect waves-light blue">Afficher</a>
-				</div>
-			</li>
+	<aside class="col s12 m3">
+		<ul class="collection">
+			<li class="collection-item active"><div>Carnet de bord<a href="#!" class="secondary-content"></a></div></li>
+			<li class="collection-item"><div>Correcteurs<a href="#!" class="secondary-content"></a></div></li>
+			<li class="collection-item"><div>Leçons<a href="#!" class="secondary-content"></a></div></li>
 		</ul>
-	</div>
-	<div class="col s12 m9">
+	</aside>
+
+	<article class="col s12 m9">
 		<h1>Carnet de bord</h1>
+		<div class="card-panel blue lighten-5 row">
+			<p class="col s6 m3"><span class="label">Début : </span><input id="debut" name="debut" type="date" value="" class="datepicker" /></p>
+			<p class="col s6 m3"><span class="label">Fin : </span><input id="fin" name="fin" type="date" value="" class="datepicker" /></p>
+			<p class="col s6 m3"><span class="label">Type : </span><input type="text" name="type" value="" /></p>
+			<p class="col s6 m3"><span class="label">Utilisateur : </span><input type='text' name='persono_id' value="" /></p>
+			<p class="col s12"><a href="" class="btn waves-effect waves-light blue right">Afficher</a></p>
+		</div>
+
 		<table class="striped">
 			<th><td>Date</td><td>Personne</td><td>Type</td><td>Message</td></th>
 			<tr><td>&nbsp;</td><td>14/07/2017</td><td>584</td><td>Envoi RAPPEL</td><td>Envoi d'un message de rappel après 6 jours</td></tr>
@@ -48,7 +34,8 @@ include "pagxkapo.inc.php";
 			<tr><td>&nbsp;</td><td>14/07/2017</td><td>584</td><td>Envoi RAPPEL</td><td>Envoi d'un message de rappel après 6 jours</td></tr>
 			<tr><td>&nbsp;</td><td>14/07/2017</td><td>584</td><td>Envoi RAPPEL</td><td>Envoi d'un message de rappel après 6 jours</td></tr>
 		</table>
-	</div>
+	</article>
+
 			
 </div>
 <?php include "pagxpiedo.inc.php"; ?>

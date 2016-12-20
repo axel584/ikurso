82ea2829e8bd045b2791f2694f8d011c52e79f23
axel584 <axel584@gmail.com>
2016-12-20 22:30:23 +0100

    On sépare le script diplome.php en deux, l'un fait le code html et l'autre l'image

--- diplome.php
+++ diplome.php
@@ -23,52 +23,6 @@ if ($infos==null) {
 	// on n'a pas trouvé de cours finis pour cet élève (tentative de fraude ?)
 	header("Location:personinformoj.php?erarkodo=17");	
 }
-// Afficher le résultat en PDF
-
-//header ("Content-type: image/png");
-if ($infos["kurso"]=="CG") {
-	$image = imagecreatefrompng("bildoj/diplomeCG.png");
-} elseif ($infos["kurso"]=="KE") {
-	$image = imagecreatefrompng("bildoj/diplomeKE.png");
-} elseif ($infos["kurso"]=="GR") {
-	$image = imagecreatefrompng("bildoj/diplomeGR.png");
-}
-
-
-// gestion des couleurs
-	$blanc = imagecolorallocate($image,hexdec("FF"),hexdec("FF"),hexdec("FF"));
-	$noir = imagecolorallocate($image,hexdec("00"),hexdec("00"),hexdec("00"));
-if ($kurso=="CG") {
-	// on découpe la couleur #4A90E2 en rouge/vert/bleu :
-	$couleur = imagecolorallocate($image,hexdec("4A"),hexdec("90"),hexdec("E2"));
-} elseif ($kurso=="KE") {
-	// on découpe la couleur #4A90E2 en rouge/vert/bleu :
-	$couleur = imagecolorallocate($image,hexdec("4A"),hexdec("90"),hexdec("E2"));
-} elseif ($kurso=="GR") {
-	// on découpe la couleur #68AC1D en rouge/vert/bleu :
-	$couleur = imagecolorallocate($image,hexdec("68"),hexdec("AC"),hexdec("1D"));
-}
-
-// si on a un nom de famille ou un prénom on l'utilise pour afficher sur le diplome, sinon on prend l'identifiant
-if ($persono["familinomo"]!="" || $persono["personnomo"]!="") {
-	$nom = $persono["personnomo"]." ".$persono["familinomo"];
-} else {
-	$nom = $persono["enirnomo"];
-}
-
-// remplissage du texte
-imagettftext($image, 16, 0, 300, 30, $couleur, 'fonts/OpenSans-Light.ttf', 'Parizo, la '.$infos["findato"]);
-if (strlen($nom)>18) {
-	imagettftext($image, 32, 0, 300, 300, $noir, 'fonts/OpenSans-Light.ttf', $nom);
-} else {
-	imagettftext($image, 38, 0, 300, 300, $noir, 'fonts/OpenSans-Light.ttf', $nom);
-}
-//imagettftext($image, 12, 0, 600, 500, $couleur, 'fonts/OpenSans-Light.ttf', 'instruisto korektanto:');
-imagettftext($image, 16, 0, 600, 570, $couleur, 'fonts/OpenSans-Regular.ttf', $infos["personnomo"]." ".$infos["familinomo"]); // nom-prénom du correcteur
-ob_start();
-imagepng($image);
-$imagedata = ob_get_contents();
-ob_end_clean();
 ?>
 <html>
 	<head>
@@ -83,7 +37,13 @@ ob_end_clean();
 		<link type="text/css" rel="stylesheet" href="style/impression.css" media="print"/>
 	</head>
     <body class="attestation">
-		<?php echo '<img src="data:image/png;base64,'.base64_encode($imagedata).'" width="842" height="595"/>'; ?>
+		<?php 
+		if ($clef!="") {
+			$urlClef = "&clef=".$clef;
+		} else {
+			$urlClef = "";
+		}
+		echo '<img src="diplomeImage.php?kurso='.$kurso.$urlClef.'" width="842" height="595"/>'; ?>
 		<div class="outils">
 			<a class="btn waves-effect waves-light blue modal-trigger imprimer" href="" onclick="window.print();">Imprimer l’attestation</a>
 			<a class="btn waves-effect waves-light blue modal-trigger fermer" href="" onclick="window.close();">Fermer</a>

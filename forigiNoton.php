091ae57db332e24baa48fd77a06446054a533c87
axel584 <axel584@gmail.com>
2017-06-05 11:01:42 +0200

    Fix #669

--- forigiNoton.php
+++ forigiNoton.php
@@ -4,7 +4,7 @@ $noto_id=isset($_GET["noto_id"])?$_GET["noto_id"]:"";
 $pagxo=isset($_GET["pagxo"])?$_GET["pagxo"]:"";
 malfermidatumbazon();
 $demando = "delete from komentoj where id='$noto_id'";
-echo $demando;
+//echo $demando;
 $bdd->exec($demando) or die(print_r($bdd->errorInfo()));
 header("Location:".$pagxo."");
 ?>

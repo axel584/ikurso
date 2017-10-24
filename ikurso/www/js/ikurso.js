b34e1884b90cdc07af94d2e9ce0dd1d924f3aa5e
axel584 <axel584@gmail.com>
2017-10-24 22:24:55 +0200

    scroll la page en haut

--- ikurso/www/js/ikurso.js
+++ ikurso/www/js/ikurso.js
@@ -9,9 +9,13 @@ $(document).ready(function() {
 			$(".btn").click(function () {
 			var target = $(this).data("target");
 			charge(target);
+			$("body").scrollTop(0); // sans animation
+			//$("body").animate({scrollTop:0}, '500', 'swing'); // avec animation
 			});
 		});
+
 	};
+
 
 
 

0c1cf8772bb752859c193b6a3a7204ebcba9f2cf
axel584 <axel584@gmail.com>
2018-01-11 14:27:00 +0100

    correction dans la syntaxe html

--- fr/cge/vocabula.php
+++ fr/cge/vocabula.php
@@ -11,5 +11,7 @@ include "dlekkapo.inc.php";
 		<section id="leciono-enhavo">
 		<h2>Lexique</h2>
 	<?php include "vortlisto.inc.php"; ?>
+	</section>
 	</article>
+</div>	
 <?php include "dlekpiedo.inc.php"; ?>

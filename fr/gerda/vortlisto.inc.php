e4d97e11d1e6acb4b69840e87bc16f7006733745
axel584 <axel584@gmail.com>
2018-01-11 14:26:44 +0100

    Fix #776

---
+++ fr/gerda/vortlisto.inc.php
@@ -0,0 +1,25 @@
+			<p>Ce lexique contient en principe tous les mots nécessaires pour les
+			exercices, plus quelques autres à titre d’information. Attention: les
+			mots choisis ici sont ceux qui se traduisent par une racine simple en
+			espéranto. Vous y trouverez par exemple <em>bon</em>, mais pas <em>mauvais</em>, car
+			<span class="eo">malbona</span> est un dérivé de <span class="eo">bona</span>. Cherchez donc les contraires, les
+			verbes, les noms, etc. ayant un sens apparenté au mot que vous cherchez.</p>
+			<p class="rimarko"><strong>NOTE</strong> : <br>
+			Les chiffres entre parenthèses renvoient à la leçon où le mot
+			est introduit ou expliqué (pas toujours celle où il apparaît la
+			première fois). Le premier chiffre fait référence au numéro 
+			de la leçon du <em>Cours en dix leçons</em>, le deuxième chiffre fait 
+			référence au numéro de la leçon du <em>Kurso de Esperanto</em>.
+			</p>
+			<div class="row">
+				<div class="input-field col s6 l4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="tapez les premières lettres du mot recherché, sans les accents">
+					<i class="material-icons prefix">search</i><input type="text" name="pattern" data-kurso="GR" id="pattern">
+					<label for="pattern">chercher dans le lexique</label>
+				</div>
+			</div>
+<?php
+
+	vortlisto($persono_id,"GR","");
+
+?>
+

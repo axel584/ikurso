f917ac8da74bdf2d382f8749f683f645ad757297
Emmanuelle Richard <emmrichard@gmail.com>
2018-01-24 20:27:03 +0100

    correction liens temaro fix #777

--- fr/cge/temaro.php
+++ fr/cge/temaro.php
@@ -17,25 +17,21 @@ include "dlekkapo.inc.php";
 					<h3>Prépositions</h3>
 					<ul>
 						<li><a href="lec01.php?section=7">Préposition <b>en</b></a>
-						<li><a href="lec02.pho?section=4">Prépositions <b>antaŭ</b>, <b>sub</b>, <b>sur</b>, <b>super</b></a>
-						<li><a href="lec06.php?section=6">Révision du premier bloc</a></li>
-						<li><a href="lec06.php?section=8">Révision du deuxième bloc</a></li>
-						<li><a href="lec09.php?section=4">Prépositions en <b>-aŭ</b></a></li>
+						<li><a href="lec02.php?section=4">Prépositions <b>antaŭ</b>, <b>sub</b>, <b>sur</b>, <b>super</b></a>
+						<li><a href="lec06.php?section=6">Révision des prépositions</a></li>
 					</ul>
 					
 					<h3>Nombres</h3>
 					<ul>
 						<li><a href="lec03.php?section=8">Nombres cardinaux</a></li>
 						<li><a href="lec03.php?section=8#ordinaux">Nombres ordinaux</a></li>
-						<li><a href="lec09.php?section=4"><b>ambaŭ</b></a></li>
 					</ul>
 				
 					<h3>Pronoms</h3>
 					<ul>
 						<li><a href="lec01.php?section=4">Pronoms personnels</a></li>
 						<li><a href="lec05.php?section=2">Pronoms corrélatifs en <b>-a</b>, <b>-u</b> et <b>-o</b></a></li>
-						<li><a href="lec08.php?section=5">Pronom réfléchi</a></li>
-						<li><a href="lec09.php?section=4"><b>ambaŭ</b></a></li>
+						<li><a href="lec08.php?section=6">Pronom réfléchi</a></li>
 					</ul>
 					
 					<h3>Petits mots</h3>
@@ -43,13 +39,13 @@ include "dlekkapo.inc.php";
 						<li><a href="lec01.php?section=4#articles">Articles</a></li>
 						<li><a href="lec05.php?section=2">Corrélatifs</a></li>
 						<li><a href="lec01.php?section=9"><b>kiu</b>, <b>kie</b></a></li>
-						<li><a href="lec02.php?section=11"><b>kiel</b></a></li>
+						<li><a href="lec02.php?section=12"><b>kiel</b></a></li>
 						<li><a href="lec03.php?section=12"><b>kio</b></a></li>
 						<li><a href="lec03.php?section=8"><b>kiom</b></a></li>
 						<li><a href="lec09.php?section=5"><b>ĉi</b></a></li>
 						<li><a href="lec09.php?section=5"><b>ajn</b></a></li>
-						<li><a href="lec02.php?section=6"><b>neniam</b>, <b>nenie</b>, <b>neniu</b>, <b>nenio</b></a></li>
-						<li><a href="lec02.php?section=1"><b>tiel</b></a></li>
+						<li><a href="lec02.php?section=7"><b>neniam</b>, <b>nenie</b>, <b>neniu</b>, <b>nenio</b></a></li>
+						<li><a href="lec02.php?section=12"><b>tiel</b></a></li>
 					</ul>
 					<h3>Verbes</h3>
 					<ul>
@@ -66,7 +62,7 @@ include "dlekkapo.inc.php";
 						<li><a href="lec01.php?section=4">Pluriel</a></li>
 						<li><a href="lec01.php?section=4">Adjectifs possessifs</a></li>
 						<li><a href="lec05.php?section=2">Adjectifs corrélatifs en <b>-a</b>, <b>-u</b> et <b>-es</b></a></li>
-						<li><a href="lec08.php?section=5">Adjectif possessif réfléchi</a></li>
+						<li><a href="lec08.php?section=6">Adjectif possessif réfléchi</a></li>
 					</ul>
 				</div>
 				<div class="col s12 m6">
@@ -79,8 +75,8 @@ include "dlekkapo.inc.php";
 					</ul>
 					<h3>Dérivation</h3>
 					<ul>
-						<li><a href="lec02.php?section=1">Féminin</a></li>
-						<li><a href="lec02.php?section=4">Préfixe <b>mal-</b></a></li>
+						<li><a href="lec02.php?section=2">Féminin</a></li>
+						<li><a href="lec02.php?section=6">Préfixe <b>mal-</b></a></li>
 						<li><a href="lec07.php?section=1">Préfixes</a></li>
 						<li><a href="lec07.php?section=2">Suffixes</a></li>
 					</ul>
@@ -89,20 +85,20 @@ include "dlekkapo.inc.php";
 						<li><a href="lec01.php">Généralités</a></li>
 						<li><a href="lec01.php?section=1">Accent tonique</a></li>
 						<li><a href="lec04.php?section=3">Alphabet</a></li>
-						<li><a href="lec04.php?section=6">Diphtongues</a></li>
-						<li><a href="lec06.php?section=3">Rappel sur accent tonique, <b>c</b> <b>h</b></a></li>
-						<li><a href="lec08.php?section=7">Rappel sur accent tonique, <b>h</b>, <b>r</b></a></li>
+						<li><a href="lec04.php?section=4">Diphtongues</a></li>
+						<li><a href="lec06.php?section=3">Rappel sur la prononciation de <b>c</b> <b>h</b>, <b>r</b></a></li>
+						<li><a href="lec08.php?section=9">Rappel sur l’accent tonique</a></li>
 						<li><a href="lec09.php?section=2">Élision</a></li>
 						<li><a href="lec09.php?section=4">Rappel sur <b>-aŭ</b></a></li>
 					</ul>
 					<h3>Syntaxe</h3>
 					<ul>
-						<li><a href="lec02.php?section=2">Accusatif pour complément d’objet direct</a></li>
-						<li><a href="lec02.php?section=5">Forme négative</a></li>
-						<li><a href="lec02.php?section=11">Comment ça va ? (quelques phrases utiles)</a></li>
+						<li><a href="lec02.php?section=3">Accusatif pour complément d’objet direct</a></li>
+						<li><a href="lec02.php?section=7">Forme négative</a></li>
+						<li><a href="lec02.php?section=12">Comment ça va ? (quelques phrases utiles)</a></li>
 						<li><a href="lec03.php?section=1">Ordre des mots</a></li>
 						<li><a href="lec04.php?section=1">Est-ce que… ?, oui, non</a></li>
-						<li><a href="lec05.php?section=1">Conversation</a></li>
+						<li><a href="lec05.php?section=1">Éléments pour converser</a></li>
 						<li><a href="lec06.php?section=2">Corrélatifs interrogatifs</a></li>
 						<li><a href="lec06.php?section=11">Traduction de <em>il y a</em></a></li>
 						<li><a href="lec06.php?section=13">Complément d’objet indirect</a></li>

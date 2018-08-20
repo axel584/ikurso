d0a68cf3fdf6f1bbcc3181646152f809a1bb5f2c
axel584 <axel584@gmail.com>
2018-08-20 14:53:03 +0200

    Create forigi-uzantoj-datumoj.sql

---
+++ sql/forigi-uzantoj-datumoj.sql
@@ -0,0 +1,11 @@
+TRUNCATE personoj_lecioneroj;
+TRUNCATE eraraj_lecionoj;
+TRUNCATE komentoj;
+TRUNCATE korektebla_kurso;
+TRUNCATE nuna_kurso;
+TRUNCATE personoj;
+TRUNCATE personoj_lecionoj;
+TRUNCATE personoj_vortoj;
+TRUNCATE personoj_vortoj_respondoj;
+TRUNCATE protokolo;
+TRUNCATE respondoj; 

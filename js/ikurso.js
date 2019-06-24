b7c9b6bc5f199d469f672547a38778898ef11e22
axel584 <axel584@gmail.com>
2019-06-24 19:15:36 +0200

    Fix #895 : correction du problème de point d'interrogation de manière un peu plus propre

--- js/ikurso.js
+++ js/ikurso.js
@@ -152,18 +152,13 @@ $('#eniri_identigilo,#eniri_pasvorto').keyup(function(e) {
 $(".korektebla").focusout( function () {
   $ekzercero = $(this).data('ekzercero');
   $studanto = $(this).data('studanto');
-  console.log("korektebla : "+$(this).val());
   $formulaire = $(this);
-  if ($formulaire.val()=="?") {
-    $respondo=""; // correction #895
-  } else {
-    $respondo = $formulaire.val();
-  }
+  $respondo=$formulaire.val();
           $.ajax({
           url : $cheminAbsolu+'ajax/memkorektado.php',
           type : 'GET',
           dataType : 'json',
-          data : "ekzercero="+$ekzercero+"&studanto="+$studanto+"&respondo="+$respondo,
+          data : "ekzercero="+$ekzercero+"&studanto="+$studanto+"&respondo="+encodeURIComponent($respondo),
           success : function(reponse, statut){ 
               if (reponse.mesagxo=="ok") {
                 $formulaire.addClass("valid");

b022394728a00271b81e2c0bb78ccde083e2ce46
Rousseau Axel <axel584@gmail.com>
2019-09-19 14:03:10 +0200

    ajout du script de calcul de duree

---
+++ python/calculeMedianeTempsParLecon.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+import MySQLdb
+import codecs
+import sys
+import config
+import re
+from datetime import date, time, datetime
+
+resultat = codecs.open("updateDauxro.sql","w","utf-8")
+
+connection = MySQLdb.connect (host = "127.0.0.1",user = config.user,passwd = config.passwd,db = config.db,charset = 'utf8',use_unicode=True)
+
+cursorKurso = connection.cursor()
+cursorLeciono = connection.cursor()
+cursorLecionero = connection.cursor()
+cursorTempo = connection.cursor()
+
+def arrondi(secondes) :
+    if secondes==None :
+        return
+    else :    
+        minutes = round(secondes/60)
+        if minutes>10 :
+            paquet5 = round(minutes/5)
+            minutes=paquet5*5
+        return minutes
+    
+
+def mediane(tableau) :
+    if len(tableau)==0 :
+        return
+    indice_median = round(len(tableau)/2)
+    if indice_median==0 :
+        return
+    tableau.sort()    
+    print(tableau)
+    return tableau[indice_median-1]    
+
+cursorKurso.execute("SELECT id,kodo FROM kursoj")
+for id_kurso,kodo_kurso in cursorKurso.fetchall() :
+    print("kurso : "+kodo_kurso)
+    cursorLeciono.execute("select id,numero,titolo from lecionoj where kurso='"+kodo_kurso+"' order by numero")
+    for id_leciono,numero,titolo in cursorLeciono.fetchall() :
+        print ("leciono : "+str(numero))
+        cursorLecionero.execute("SELECT id,titolo,dauxro FROM lecioneroj WHERE leciono_id="+str(id_leciono)+" order by ordo")
+        for id_lecionero,titolo_lecionero,dauxro_lecionero in cursorLecionero.fetchall() :
+            print("id_lecionero : "+str(id_lecionero)+" / lecionero : "+titolo_lecionero.encode('utf-8').decode('ascii','ignore'))
+            cursorTempo.execute("SELECT dato-ekdato as dauxro FROM personoj_lecioneroj WHERE lecionero_id="+str(id_lecionero)+" and ekdato is not null")
+            dauxroj = []
+            for dauxro, in cursorTempo.fetchall():
+                dauxroj.append(dauxro)
+            if len(dauxroj)<2 :
+                continue
+            valeur_mediane = mediane(dauxroj)
+            if valeur_mediane==None :
+                continue
+            nova_dauxro = arrondi(valeur_mediane)
+            print("ancien :"+str(dauxro_lecionero)+"/nouvelle : "+str(nova_dauxro))
+            resultat.write("-- "+titolo_lecionero+" ("+str(dauxro_lecionero)+")\n")
+            resultat.write("update lecioneroj set dauxro="+str(nova_dauxro)+" where id="+str(id_lecionero)+";\n")
+            
+            
---
+++ python/updateDauxro.sql
@@ -0,0 +1,458 @@
+-- Indications sur la prononciation (5)
+update lecioneroj set dauxro=4 where id=1;
+-- Les terminaisons usuelles (5)
+update lecioneroj set dauxro=2 where id=2;
+-- Vocabulaire (6)
+update lecioneroj set dauxro=4 where id=3;
+-- Un peu de grammaire... (10)
+update lecioneroj set dauxro=6 where id=4;
+-- Exemples de phrases (5)
+update lecioneroj set dauxro=2 where id=5;
+-- Exercice 1 (8)
+update lecioneroj set dauxro=10 where id=6;
+-- Mes premiers mots (7)
+update lecioneroj set dauxro=3 where id=219;
+-- Qui est qui ? (11)
+update lecioneroj set dauxro=10 where id=253;
+-- Premières questions (5)
+update lecioneroj set dauxro=5 where id=269;
+-- Exercices 2 et 3 (17)
+update lecioneroj set dauxro=20 where id=270;
+-- Je me présente (12)
+update lecioneroj set dauxro=20 where id=254;
+-- QCM (4)
+update lecioneroj set dauxro=4 where id=7;
+-- Épilogue et envoi des exercices (5)
+update lecioneroj set dauxro=2 where id=8;
+-- Récapitulatif de la leçon 1 (7)
+update lecioneroj set dauxro=3 where id=218;
+-- Le féminin des noms : suffixe <i>-in</i> (6)
+update lecioneroj set dauxro=2 where id=9;
+-- Le complément d’objet direct (10)
+update lecioneroj set dauxro=15 where id=10;
+-- Autres compléments (6)
+update lecioneroj set dauxro=8 where id=11;
+-- Exercice 1 (2)
+update lecioneroj set dauxro=6 where id=276;
+-- Préfixe <i>mal-</i> (3)
+update lecioneroj set dauxro=3 where id=12;
+-- Forme négative (5)
+update lecioneroj set dauxro=7 where id=13;
+-- Vocabulaire (6)
+update lecioneroj set dauxro=5 where id=14;
+-- Exercice 2 (18)
+update lecioneroj set dauxro=25 where id=15;
+-- Exercice 3 (7)
+update lecioneroj set dauxro=9 where id=16;
+-- Exercice 4 (7)
+update lecioneroj set dauxro=10 where id=17;
+-- Comment ça va ? (6)
+update lecioneroj set dauxro=7 where id=221;
+-- QCM (4)
+update lecioneroj set dauxro=4 where id=18;
+-- Épilogue et envoi des exercices (5)
+update lecioneroj set dauxro=1 where id=19;
+-- Ordre des mots (7)
+update lecioneroj set dauxro=2 where id=20;
+-- Exercice 1 (5)
+update lecioneroj set dauxro=4 where id=21;
+-- Vocabulaire (3)
+update lecioneroj set dauxro=5 where id=22;
+-- Proverbe (2)
+update lecioneroj set dauxro=1 where id=23;
+-- Adverbes en <i>-e</i> (7)
+update lecioneroj set dauxro=5 where id=24;
+-- Formation des syllabes (1)
+update lecioneroj set dauxro=1 where id=25;
+-- Exercice 2 (8)
+update lecioneroj set dauxro=7 where id=26;
+-- Les nombres cardinaux (7)
+update lecioneroj set dauxro=10 where id=27;
+-- Exercice 3 (13)
+update lecioneroj set dauxro=15 where id=28;
+-- Verbes intransitifs et transitifs (5)
+update lecioneroj set dauxro=3 where id=29;
+-- Exercice 4 (8)
+update lecioneroj set dauxro=15 where id=30;
+-- Qu'est-ce que c'est ? (4)
+update lecioneroj set dauxro=5 where id=274;
+-- Exercice 5 (8)
+update lecioneroj set dauxro=10 where id=275;
+-- Épilogue et envoi des exercices (5)
+update lecioneroj set dauxro=0 where id=31;
+-- La forme interrogative (10)
+update lecioneroj set dauxro=5 where id=32;
+-- Exercice 1 (4)
+update lecioneroj set dauxro=3 where id=33;
+-- L’alphabet (rappel) (6)
+update lecioneroj set dauxro=6 where id=34;
+-- Les diphtongues (3)
+update lecioneroj set dauxro=2 where id=37;
+-- Vocabulaire (10)
+update lecioneroj set dauxro=10 where id=38;
+-- Encore des compléments (6)
+update lecioneroj set dauxro=8 where id=277;
+-- Exercice 2 (5)
+update lecioneroj set dauxro=6 where id=278;
+-- La date (9)
+update lecioneroj set dauxro=8 where id=271;
+-- Exercice 3 : la date (12)
+update lecioneroj set dauxro=20 where id=272;
+-- Récit (5)
+update lecioneroj set dauxro=5 where id=39;
+-- Exercice 4 : pratique du vocabulaire (10)
+update lecioneroj set dauxro=15 where id=40;
+-- Exercices 5 et 6 (25)
+update lecioneroj set dauxro=30 where id=41;
+-- La chanson espérantophone (7)
+update lecioneroj set dauxro=5 where id=273;
+-- Éléments pour converser (7)
+update lecioneroj set dauxro=6 where id=43;
+-- Les corrélatifs (12)
+update lecioneroj set dauxro=15 where id=44;
+-- Un peu de pratique (3)
+update lecioneroj set dauxro=4 where id=45;
+-- La flexion des corrélatifs (5)
+update lecioneroj set dauxro=3 where id=46;
+-- Proverbe (1)
+update lecioneroj set dauxro=1 where id=47;
+-- Exercices 1 et 2 (25)
+update lecioneroj set dauxro=55 where id=48;
+-- Littérature et espéranto (None)
+update lecioneroj set dauxro=3 where id=282;
+-- Révision des terminaisons (6)
+update lecioneroj set dauxro=1 where id=50;
+-- Comment poser des questions (4)
+update lecioneroj set dauxro=2 where id=51;
+-- Rappel sur la prononciation (3)
+update lecioneroj set dauxro=3 where id=52;
+-- Exercice 1 (5)
+update lecioneroj set dauxro=9 where id=53;
+-- Proverbe (3)
+update lecioneroj set dauxro=2 where id=54;
+-- Les prépositions (8)
+update lecioneroj set dauxro=8 where id=55;
+-- Exercice 2 (16)
+update lecioneroj set dauxro=20 where id=57;
+-- Premier récit et vocabulaire (7)
+update lecioneroj set dauxro=9 where id=56;
+-- Second récit et vocabulaire (6)
+update lecioneroj set dauxro=5 where id=58;
+-- Exercice 3 (17)
+update lecioneroj set dauxro=95 where id=59;
+-- Traduction de "il y a" (2)
+update lecioneroj set dauxro=1 where id=60;
+-- Exercice 4 (15)
+update lecioneroj set dauxro=20 where id=61;
+-- Complément d’objet indirect (4)
+update lecioneroj set dauxro=1 where id=62;
+-- L’accusatif peut remplacer certaines prépositions (4)
+update lecioneroj set dauxro=3 where id=63;
+-- Exercice 5 (11)
+update lecioneroj set dauxro=20 where id=64;
+-- Adverbes de lieu indiquant le mouvement (6)
+update lecioneroj set dauxro=3 where id=65;
+-- Exercice 6 (9)
+update lecioneroj set dauxro=9 where id=66;
+-- Préfixes (21)
+update lecioneroj set dauxro=7 where id=68;
+-- Suffixes (16)
+update lecioneroj set dauxro=20 where id=69;
+-- Exercices 1 et 2 (20)
+update lecioneroj set dauxro=100 where id=70;
+-- Proverbes (7)
+update lecioneroj set dauxro=5 where id=71;
+-- Expression de l’heure (12)
+update lecioneroj set dauxro=8 where id=72;
+-- Exercices 3 et 4 (10)
+update lecioneroj set dauxro=25 where id=220;
+-- Vocabulaire (2)
+update lecioneroj set dauxro=3 where id=279;
+-- Le reste de la conjugaison... (7)
+update lecioneroj set dauxro=20 where id=74;
+-- Proverbes (3)
+update lecioneroj set dauxro=3 where id=75;
+-- Exercice 1 (14)
+update lecioneroj set dauxro=20 where id=280;
+-- Exercice 2 (28)
+update lecioneroj set dauxro=40 where id=76;
+-- Exercice 3 (20)
+update lecioneroj set dauxro=15 where id=77;
+-- Pronom personnel et adjectif possessif réfléchis (3)
+update lecioneroj set dauxro=9 where id=78;
+-- Exercice 4 (15)
+update lecioneroj set dauxro=8 where id=281;
+-- Exercice 5 (25)
+update lecioneroj set dauxro=30 where id=79;
+-- Retour sur l’accent tonique (4)
+update lecioneroj set dauxro=3 where id=80;
+-- Un air connu... (9)
+update lecioneroj set dauxro=8 where id=82;
+-- L’élision (2)
+update lecioneroj set dauxro=1 where id=83;
+-- Quelques adverbes primaires en <i>-aŭ</i> (6)
+update lecioneroj set dauxro=4 where id=84;
+-- Divers mots en <i>-aŭ</i> (4)
+update lecioneroj set dauxro=2 where id=85;
+-- Autres mots invariables (4)
+update lecioneroj set dauxro=2 where id=86;
+-- Proverbes (4)
+update lecioneroj set dauxro=4 where id=87;
+-- Les revues en espéranto (10)
+update lecioneroj set dauxro=3 where id=88;
+-- Exercice (32)
+update lecioneroj set dauxro=70 where id=89;
+-- Examen (45)
+update lecioneroj set dauxro=80 where id=90;
+-- Rakonto (15)
+update lecioneroj set dauxro=3 where id=91;
+-- Klarigoj (8)
+update lecioneroj set dauxro=5 where id=92;
+-- Vortlisto (10)
+update lecioneroj set dauxro=9 where id=93;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=30 where id=94;
+-- Lasu min… (15)
+update lecioneroj set dauxro=6 where id=95;
+-- Sendi la ekzercojn (5)
+update lecioneroj set dauxro=1 where id=222;
+-- Rakonto (15)
+update lecioneroj set dauxro=9 where id=96;
+-- Klarigoj (8)
+update lecioneroj set dauxro=4 where id=97;
+-- Vortlisto (10)
+update lecioneroj set dauxro=8 where id=98;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=35 where id=99;
+-- Lasu min… (15)
+update lecioneroj set dauxro=7 where id=100;
+-- Rakonto (15)
+update lecioneroj set dauxro=8 where id=101;
+-- Klarigoj (8)
+update lecioneroj set dauxro=3 where id=102;
+-- Vortlisto (10)
+update lecioneroj set dauxro=6 where id=103;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=20 where id=104;
+-- Lasu min… (15)
+update lecioneroj set dauxro=10 where id=105;
+-- Rakonto (15)
+update lecioneroj set dauxro=15 where id=106;
+-- Klarigoj (8)
+update lecioneroj set dauxro=1 where id=107;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=108;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=20 where id=109;
+-- Lasu min… (15)
+update lecioneroj set dauxro=10 where id=110;
+-- Rakonto (15)
+update lecioneroj set dauxro=5 where id=111;
+-- Klarigoj (8)
+update lecioneroj set dauxro=2 where id=112;
+-- Vortlisto (10)
+update lecioneroj set dauxro=6 where id=113;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=15 where id=114;
+-- Lasu min… (15)
+update lecioneroj set dauxro=10 where id=115;
+-- Rakonto (15)
+update lecioneroj set dauxro=4 where id=116;
+-- Klarigoj (8)
+update lecioneroj set dauxro=4 where id=117;
+-- Vortlisto (10)
+update lecioneroj set dauxro=1 where id=118;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=10 where id=119;
+-- Lasu min… (15)
+update lecioneroj set dauxro=10 where id=120;
+-- Rakonto (15)
+update lecioneroj set dauxro=9 where id=121;
+-- Klarigoj (8)
+update lecioneroj set dauxro=5 where id=122;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=123;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=30 where id=124;
+-- Lasu min… (15)
+update lecioneroj set dauxro=15 where id=125;
+-- Rakonto (15)
+update lecioneroj set dauxro=8 where id=126;
+-- Klarigoj (8)
+update lecioneroj set dauxro=7 where id=127;
+-- Vortlisto (10)
+update lecioneroj set dauxro=4 where id=128;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=45 where id=129;
+-- Lasu min… (15)
+update lecioneroj set dauxro=1 where id=130;
+-- Rakonto (15)
+update lecioneroj set dauxro=15 where id=131;
+-- Klarigoj (8)
+update lecioneroj set dauxro=5 where id=132;
+-- Vortlisto (10)
+update lecioneroj set dauxro=5 where id=133;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=20 where id=134;
+-- Lasu min… (15)
+update lecioneroj set dauxro=3 where id=135;
+-- Rakonto (15)
+update lecioneroj set dauxro=80 where id=136;
+-- Klarigoj (8)
+update lecioneroj set dauxro=3 where id=137;
+-- Vortlisto (10)
+update lecioneroj set dauxro=2 where id=138;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=40 where id=139;
+-- Lasu min… (15)
+update lecioneroj set dauxro=7 where id=140;
+-- Rakonto (15)
+update lecioneroj set dauxro=9 where id=141;
+-- Klarigoj (8)
+update lecioneroj set dauxro=2 where id=142;
+-- Vortlisto (10)
+update lecioneroj set dauxro=2 where id=143;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=35 where id=144;
+-- Lasu min… (15)
+update lecioneroj set dauxro=2 where id=145;
+-- Rakonto (15)
+update lecioneroj set dauxro=10 where id=146;
+-- Klarigoj (8)
+update lecioneroj set dauxro=7 where id=147;
+-- Vortlisto (10)
+update lecioneroj set dauxro=2 where id=148;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=25 where id=149;
+-- Lasu min… (15)
+update lecioneroj set dauxro=15 where id=150;
+-- Rakonto (15)
+update lecioneroj set dauxro=9 where id=151;
+-- Klarigoj (8)
+update lecioneroj set dauxro=2 where id=152;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=153;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=105 where id=154;
+-- Lasu min… (15)
+update lecioneroj set dauxro=30 where id=155;
+-- Rakonto (15)
+update lecioneroj set dauxro=25 where id=156;
+-- Klarigoj (8)
+update lecioneroj set dauxro=4 where id=157;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=158;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=70 where id=159;
+-- Lasu min… (15)
+update lecioneroj set dauxro=9 where id=160;
+-- Rakonto (15)
+update lecioneroj set dauxro=15 where id=161;
+-- Klarigoj (8)
+update lecioneroj set dauxro=1 where id=162;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=163;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=95 where id=164;
+-- Lasu min… (15)
+update lecioneroj set dauxro=45 where id=165;
+-- Rakonto (15)
+update lecioneroj set dauxro=7 where id=166;
+-- Klarigoj (8)
+update lecioneroj set dauxro=15 where id=167;
+-- Vortlisto (10)
+update lecioneroj set dauxro=2 where id=168;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=95 where id=169;
+-- Lasu min… (15)
+update lecioneroj set dauxro=25 where id=170;
+-- Rakonto (15)
+update lecioneroj set dauxro=15 where id=171;
+-- Klarigoj (8)
+update lecioneroj set dauxro=15 where id=172;
+-- Vortlisto (10)
+update lecioneroj set dauxro=8 where id=173;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=90 where id=174;
+-- Lasu min… (15)
+update lecioneroj set dauxro=100 where id=175;
+-- Rakonto (15)
+update lecioneroj set dauxro=25 where id=176;
+-- Klarigoj (8)
+update lecioneroj set dauxro=4 where id=177;
+-- Vortlisto (10)
+update lecioneroj set dauxro=5 where id=178;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=80 where id=179;
+-- Lasu min… (15)
+update lecioneroj set dauxro=85 where id=180;
+-- Rakonto (15)
+update lecioneroj set dauxro=7 where id=181;
+-- Klarigoj (8)
+update lecioneroj set dauxro=0 where id=182;
+-- Vortlisto (10)
+update lecioneroj set dauxro=0 where id=183;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=110 where id=184;
+-- Lasu min… (15)
+update lecioneroj set dauxro=6 where id=185;
+-- Rakonto (15)
+update lecioneroj set dauxro=10 where id=186;
+-- Klarigoj (8)
+update lecioneroj set dauxro=0 where id=187;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=188;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=135 where id=189;
+-- Lasu min… (15)
+update lecioneroj set dauxro=10 where id=190;
+-- Rakonto (15)
+update lecioneroj set dauxro=15 where id=191;
+-- Klarigoj (8)
+update lecioneroj set dauxro=0 where id=192;
+-- Vortlisto (10)
+update lecioneroj set dauxro=20 where id=193;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=135 where id=194;
+-- Lasu min… (15)
+update lecioneroj set dauxro=95 where id=195;
+-- Rakonto (15)
+update lecioneroj set dauxro=75 where id=196;
+-- Klarigoj (8)
+update lecioneroj set dauxro=2 where id=197;
+-- Vortlisto (10)
+update lecioneroj set dauxro=4 where id=198;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=130 where id=199;
+-- Lasu min… (15)
+update lecioneroj set dauxro=35 where id=200;
+-- Rakonto (15)
+update lecioneroj set dauxro=5 where id=201;
+-- Klarigoj (8)
+update lecioneroj set dauxro=15 where id=202;
+-- Vortlisto (10)
+update lecioneroj set dauxro=3 where id=203;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=60 where id=204;
+-- Lasu min… (15)
+update lecioneroj set dauxro=2 where id=205;
+-- Rakonto (15)
+update lecioneroj set dauxro=1 where id=206;
+-- Klarigoj (8)
+update lecioneroj set dauxro=15 where id=207;
+-- Vortlisto (10)
+update lecioneroj set dauxro=1 where id=208;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=115 where id=209;
+-- Lasu min… (15)
+update lecioneroj set dauxro=0 where id=210;
+-- Rakonto (15)
+update lecioneroj set dauxro=10 where id=211;
+-- Klarigoj (8)
+update lecioneroj set dauxro=0 where id=212;
+-- Vortlisto (10)
+update lecioneroj set dauxro=1 where id=213;
+-- Ekzercoj (20)
+update lecioneroj set dauxro=75 where id=214;
+-- Lasu min… (15)
+update lecioneroj set dauxro=2 where id=215;

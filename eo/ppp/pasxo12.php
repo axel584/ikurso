7aeeebd5d122968d3171383d0e1e9ffa3858a043
Emmanuelle Richard <emmrichard@gmail.com>
2021-10-08 00:07:01 +0200

    pasxo 12

---
+++ eo/ppp/pasxo12.php
@@ -0,0 +1,227 @@
+<?php 
+$titolo="Paŝo 12 (dekdua)";
+$leciono = 12;
+$section=isset($_GET["section"])?htmlspecialchars($_GET["section"]):"1";
+include "pppkapo.inc.php";
+?>
+<div class="row">
+	<article class="col s12 m10 l7 offset-m1 offset-l1">
+		
+		<section id="leciono-enhavo">
+		<?php 
+		getTitoloLecionero('PP',$leciono,$section);
+		?>
+		<?php
+		if ($section=="1") {
+		?>
+
+		<div class="rakonto">
+			<h3>DIA MASTRUMADO</h3>
+ 
+ 			<p>(Parolas Peruno, dio de la antikvaj slavoj:)</p>
+
+ 			<p>“Vi sensperte vivas certe,<br />
+ 			neestinte dio.<br />
+ 			False ŝajnas, ke mi gajnas<br />
+ 			en la profesio!</p>
+
+ 			<p>Mi matene devas pene<br />
+ 			rosi kampan valon,<br />
+ 			hejti sunon, kaj la lunon<br />
+ 			fermi en la stalon.</p>
+ 			
+ 			<p>Noktfantomojn kaj demonojn<br />
+ 			en la sakon loki,<br />
+ 			la stelflokojn kiel kokojn<<br />
+ 			al nestejo voki.</p>
+ 			
+ 			<p>Ĉies buŝo: birdo, muŝo,<br />
+ 			pulo, elefanto,<br />
+ 			frue jam ĝi volas manĝi<br />
+ 			el di-provianto.</p>
+ 			
+ 			<p>La ekstaro de l’ homaro,<br />
+ 			aĥ, ĝi kaŭzas servojn!<br />
+ 			La lamento de l’ homgento<br />
+ 			ŝiras miajn nervojn!</p>
+ 			
+ 			<p>Nur se pikoj de formikoj<br />
+ 			vian aŭdon ĝenus,<br />
+ 			la matenan audiencon<br />
+ 			eble vi komprenus.</p>
+ 			
+ 			<p>Preĝoj svarmas kaj alarmas,<br />
+ 			ĝis mi preskaŭ surdas:<br />
+ 			tiu kantas, flustras, larmas,<br />
+ 			tiu psalmojn gurdas.</p>
+ 			
+ 			<p>Ĉu kalkuli la postulojn?<br />
+ 			Estus vane peni!<br />
+ 			Kapo krevus, se mi devus<br />
+ 			ĝin nur memorteni:</p>
+ 			
+ 			<p>Doni sanon kaj infanon,<br />
+ 			panon por satmanĝi;<br />
+ 			ke fabrikoj ĉesu ŝpini,<br />
+ 			devus di’ aranĝi!</p>
+ 			
+ 			<p>‘Gardu grenon kaj ĝardenon!’<br />
+ 			oni al mi taskas;<br />
+ 			nova krio: ‘Helpu dio!<br />
+ 			La bovino naskas!’ </p>
+ 			
+ 			<p>La vetero estu varma<br />
+ 			kaj samtempe frosta,<br />
+ 			la sekalo estu kara<br />
+ 			kaj malmultekosta.</p>
+ 			
+ 			<p>Pluvu! Iu semis linon,<br />
+ 			di’ ĝin rosu bene!<br />
+ 			Pluvi? Oj ne! Sed porfojne<br />
+ 			brilu sun’serene!</p>
+ 			
+ 			<p>Ve, ke estis virinaĉoj<br />
+ 			estigitaj per mi!<br />
+ 			La ĉagrenan bandon ĝenan<br />
+ 			devos mi ekstermi!</p>
+ 			
+ 			<p>Tondro ilin…! Jam eĉ dio<br />
+ 			devas ja blasfemi!<br />
+ 			Se ne laktas la kaprino,<br />
+ 			ili plendas de mi.</p>
+ 			
+ 			<p>Ĉiu vivus ne per zorgoj,<br />
+ 			sed per preĝovorto,<br />
+ 			kvazaŭ oni havus dion<br />
+ 			sole por komforto.</p>
+ 			
+ 			<p>Akvon, sekon, kampan sterkon<br />
+ 			dio disdonacu!<br />
+ 			Se porkulo sanon perdis,<br />
+ 			mi lin rekuracu!</p> 
+ 			
+ 			<p>Ŝi min tedas, de mi pledas,<br />
+ 			ke ŝin oni svatu;<br />
+ 			edzo preĝas, ke jam tondro<br />
+ 			la edzinon batu!</p> 
+ 			
+ 			<p>Kiu volas loton gajni,<br />
+ 			subaĉetas dion;<br />
+ 			kiu pagis asekuron,<br />
+ 			volas incendion!</p>
+ 			
+ 			<p>Ho, se mi ne tro indulgus<br />
+ 			vin, la hombandaĉon,<br />
+ 			mi vin premus kiel prunojn,<br />
+ 			farus el vi kaĉon!”<br />
+ 			
+ 			<p>Li enspiris naztabakon,<br />
+ 			temis bombobrue…<br />
+ 			Tondro frapis, fulmo fajris,<br />
+ 			pluvo ŝprucis flue.</p> 
+ 			
+ 			<p>Kredu, frato! Ne amuzas<br />
+ 			tiu di-ekzerco!<br />
+ 			Briksen mem — kompare tion —<br />
+ 			estas preskaŭ ŝerco.</p> 
+
+ 			<p><b>Karel Havliček Borovský</b> el la ĉeĥa: <b>Tomáš Pumpr</b> (el <i>La Bapto de Caro Vladimir</i>)</p>
+		</div>
+
+		<?php 
+		} // fin section 1
+		if ($section=="2") {
+		?>
+
+
+	<div class="vortlisto row">
+		<div class="col s12 m6">
+			<p><span class="eo">alarmo</span>: signalvoko.</p>
+			<p><span class="eo">asekuri</span>: kontrakti kontraŭ eventuala perdo.</p>
+			<p><span class="eo">audienco</span>: oficiala akcepto.</p>
+			<p><span class="eo">bando</span>: araĉo.</p>
+			<p><span class="eo">bombo</span>: eksplodaĵo.</p>
+			<p><span class="eo">bovo</span>: kampara besto, kies ino donas lakton.</p>
+			<p><span class="eo">demono</span>: diablo.</p>
+			<p><span class="eo">ekstermi</span>: tute detrui, neniigi.</p>
+			<p><span class="eo">elefanto</span>: plej granda landbesto.</p>
+			<p><span class="eo">floko</span>: malpeza peceto.</p>
+			<p><span class="eo">fojno</span>: sekigita herbo.</p>
+			<p><span class="eo">formiko</span>: speco de insekto,tre laborema.</p>
+			<p><span class="eo">gajni</span>: akiri profite.</p>
+			<p><span class="eo">greno</span>: kreskaĵo, el kiu oni faras panon.</p>
+			<p><span class="eo">gurdo</span>: muzikilo, kiun oni funkciigas per turnilo.</p>
+			<p><span class="eo">incendio</span>: brulego.</p>
+			<p><span class="eo">indulgi</span>: trakti pardoneme.</p>
+			<p><span class="eo">kaĉo</span>: manĝaĵo el faruno, kun lakto aŭ akvo.</p>
+			<p><span class="eo">kapro</span>: barbhava kampara besto.</p>
+		</div>
+		<div class="col s12 m6">			
+			<p><span class="eo">komforto</span>: ĉio, kio agrabligas la vivon.</p>
+			<p><span class="eo">muŝo</span>: fluginsekto.</p>
+			<p><span class="eo">pledi</span>: prezenti sian aferon antaŭ juĝisto.</p>
+			<p><span class="eo">loti</span>: decidi ion laŭ hazardo.</p>
+			<p><span class="eo">lino</span>: kreskaĵo, el kiu oni faras tolon.</p>
+			<p><span class="eo">porko</span>: besto, kiun oni grasigas por manĝi.</p>
+			<p><span class="eo">profesio</span>: okupo, per kiu oni vivtenas sin.</p>
+			<p><span class="eo">provianto</span>: provizoj de manĝaĵo.</p>
+			<p><span class="eo">pruno</span>: speco de frukto.</p>
+			<p><span class="eo">pulo</span>: parazita sudinsekto.</p>
+			<p><span class="eo">sako</span>: ujo el maldelikata ŝtofo.</p>
+			<p><span class="eo">sekalo</span>: speco de greno.</p>
+			<p><span class="eo">semi</span>: disĵeti grajnojn sur la teron.</p>
+			<p><span class="eo">stalo</span>: bestloĝejo.</p>
+			<p><span class="eo">sterko</span>: io uzata por plifekundigi la teron.</p>
+			<p><span class="eo">svarmi</span>: amase kunestadi kaj moviĝi.</p>
+			<p><span class="eo">svati</span>: sin proponi al iu kiel edz(in)on.</p>
+			<p><span class="eo">tabako</span>: sekigitaj folioj, kiujn oni fumas.</p>
+			<p><span class="eo">tasko</span>: difinita laboro.</p>
+			<p><span class="eo">terni</span>: brue kaj subite elspiri tra la nazo. (briksen = ekzilloko de la aŭtoro.) </p>
+		</div>
+	</div>
+
+<?php 
+		} // fin section 2
+		if ($section=="3") {
+		?>
+
+			<?php 
+				getEkzercon(152,$persono_id,"eo");
+			?>
+
+		<?php 
+		} // fin section 2
+		if ($section=="4") {
+		?>
+
+			<?php 
+				getEkzercon(153,$persono_id,"eo");	
+				getEkzercon(154,$persono_id,"eo");	
+				getEkzercon(155,$persono_id,"eo");	
+			?>                  
+<?php 
+		} // envoie de la leçon
+		if ($section=="5") {
+			recapitulatif_lecon_avant_envoi('PP',$leciono,$persono_id);
+		} // fin section 6
+		?>
+		</section>
+		
+		<section id="leciono-fino">
+			<div id="marko" class="right-align">
+				<?php getBoutonFinSection('PP',$leciono,$section,$persono_id); ?>
+				<a id="farita" class="btn-floating btn-large light-blue darken-1 hide"><i class="material-icons">done_all</i></a>
+			</div>
+			<div class="ligoj">
+				<?php getLecioneroAntauxa('PP',$leciono,$section); ?>
+				<?php getLecioneroVenonta('PP',$leciono,$section); ?>
+			</div>
+		</section>
+		
+	</article>
+	
+	<?php include "pppflanko.inc.php"; ?>
+
+</div>
+
+<?php include "ppppiedo.inc.php"; ?>			

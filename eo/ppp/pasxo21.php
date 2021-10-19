c9bb969a8186a9d1c4a66b322c6ab2b872e2d5ec
Emmanuelle Richard <emmrichard@gmail.com>
2021-10-20 01:14:52 +0200

    pashoj 20 et 21

---
+++ eo/ppp/pasxo21.php
@@ -0,0 +1,150 @@
+<?php 
+$titolo="Paŝo 21 (dudekunua)";
+$leciono = 21;
+$section=isset($_GET["section"])?htmlspecialchars($_GET["section"]):"1";
+include "pppkapo.inc.php";
+?>
+<div class="row">
+	<article class="col s12 m10 l7 offset-m1 offset-l1">
+		
+		<section id="leciono-enhavo">
+		<?php 
+		getTitoloLecionero('PP',$leciono,$section);
+		?>
+		<?php
+		if ($section=="1") {
+		?>
+
+		<div class="rakonto">
+			<h3>KONFERANSEO</h3>
+ 
+			<p>Kiam mi estis en la dua lerneja klaso ni prezentis teatraĵon kaj en ĝi mi partoprenis en rolo de popo. Ĉe ĉiu mia apero la publiko krevis pro rido kaj aplaŭdis vigle, malgraŭ ke la teatraĵo estis tragika — iu reĝfilino estas ŝtelita de ciganoj, ŝi suferas kaj mortas kiel almozulino malproksime de sia hejmo. Mi supozas, ke pri la rido kulpis mia barbo, ĉar ĉiuj komprenis, ke la barbo estas farita el kapra vosto.</p>
+			<p>Ĉe ĉiu provo diri ion mia barbo turniĝis supren, fermadis mian buŝon, tiklis mian nazon kaj mi tuj ternis. Sed plej grandan sukceson mi atingis en la fino de la teatraĵo kiam la reĝfilino mortis. Mi devis levi miajn manojn kiel eble plej rapide kaj voki Dion preni ŝian animon, kaj mi faris tion tiel energie ke mia popa mantelo ŝiriĝis (mi estis ĝin surtretinta per miaj piedoj antaŭ ol levi la manojn) kaj la barbo malkroĉiĝis kaj mole falis sur la plankon. Mi kredas, ke eĉ Kin ne atingis tian sukceson — la salono apenaŭ ne detruiĝis pro aplaŭdado kaj rido.</p>
+			<p>De tiam mi ekvivis kun la penso, ke mi estas naskita artisto. Kun paso de la tempo mi pliaĝiĝis kaj farigis helpadministranto, sed tiu penso pri mia destino ne lasis min eĉ dum minuto.</p>
+			<p>Kaj jen, hazarde, antaŭ ne longe, oni sciiĝis, ke la urba konsilantaro aranĝas grandan vesperamuzon en la salonoj de la novkonstruita modela restoracio.</p>
+			<p>Ĉio estis en ordo, sed en la lasta momento, sabate tagmeze, oni eksciis, ke forestos la konferanseo; la antaŭvidita persono ebriiĝis vendrede kaj malaperis. Sabate li sendis el urbo Sliven luksan telegramon: “Mi estas bone, kiel vi fartas”. La situacio minacis fariĝi danĝera, se mi ne enmiksiĝus. Tuj mi iris al la ĉefa urba respondeculo, kiu prizorgas la amuzon, kaj deklaris al li, ke mi estas preta konferansei, des pli ke mi havas sperton en tiaj aferoj, ja mi ludis en la lernejo la rolon de popo kaj tiel plu. Dum momento al mi ŝajnis, ke mi vidas ĝojan brilon el la okuloj de la respondeculo, sed poste li svingis per mano kaj diris:</p>
+			<p>— Mi petas vin, kamarado Conev, klopodu, ke ĉio estu iom pli… ja vi komprenas, ni ne devas nin kompromiti kaj krome la nomo de nia urba konsilantaro… ĉu vi komprenas?…</p>
+			<p>— Estu trankvila, kamarado, lasu tion al mi. Ni ja ludis en la pasinto, mi havas sufiĉan sperton, ne maltrankviliĝu!</p>
+			<p>Sed Dio scias pro kio, la respondeculo daŭre rigardis min kun certa dubo kaj malgaje balancis la kapon.</p>
+			<p>Eh, Dio mia — ĝemis li kaj al mi ŝajnis, ke li pentas pro la fakto, ke li entreprenis tiun respondecan kaj riskan aranĝon.</p>
+			<p>Sed jen la vespero venis. Mi renkontadis la gastojn ĉe la pordo, manpremis neglekte kun ĉiuj gravaj personoj de la urbo kaj kiam ili demandis min: “He, kiel estas, ha? Ĉu ni amuzos nin?”, mi svingis per la mano dirante: “Mi petas — ĉu ne mi estas la konferanseo?” kaj similajn spritaĵojn. Poste mi ekvagis inter la tabloj kaj la gastoj konkuris regali min kaj demandi pri detaloj de la programo. Al ĉiuj mi respondadis sub sekreto, ke mi aranĝis ĉion kaj estos grandaj surprizoj kaj multe da rido. Neatendite tamen la respondeculo kaptis kaj eltiris min de ĉe unu tablo, kie mi klarigas la signifon kaj la rolon de la konferanseo kaj kriis al mi:</p>
+			<p>— Kion vi faras? Jam estas la 22a horo kaj la programo ankoraŭ ne komenciĝis!?</p>
+			<p>— Kia programo? — ekmiris mi.</p>
+			<p>— Kiel kia?! — ekĝemis la respondeculo. Ja vi devas malfermi la amuzvesperon kaj prezenti la aktorojn kaj ties numerojn. La ĉestantoj jam protestas.</p>
+			<p>— Ha, estos ankaŭ numeroj, ekĝojis mi. Tre bone, saĝe elpensite.</p>
+			<p>Mi etendis mian manon por gratuli lin, sed li forpuŝis ĝin kaj montris al mi la scenejon. Meze de tondro de aplaŭdoj mi riverencis kaj sciigis per la movebla mikrofono, kiun oni enmanigis al mi, ke nia programo komenciĝas. Poste mi eksidis sur unu seĝo kaj diris al la sidantaj personoj de la najbara tablo, ke certe la programo estos tre interesa kaj mi komencis kanteti la kanton “Dornoj, knabinoj, dornoj”. En la sama momento la tuta salono ekkrevis pro rido. Mi turnis min malantaŭen kaj demandis pro kio oni ridas, sed la rido plifortiĝis. Tiam la respondeculo kriis en miajn orelojn: “Forĵetu tiun mikrofonon, sentaŭgulo!” Nur tiam mi vidis, ke mi tenas ankoraŭ la mikrofonon kaj la publiko aŭdas tion, kion mi parolas. Mi tamen devas diri, ke ankaŭ la respondeculo sin kompromitis, ĉar ĉiuj aŭdis kiel li min insultis.</p>
+			<p>Ankaŭ post tio mi ne perdis mian trankvilon kaj sciigis, ke sekvos la unua punkto de la programo. En tiu momento antaŭ mi staris unu el la flutistoj de la orkestro kaj al mia kapo tuj venis la penso profiti la okazon kaj prezenti lin kiel plenumanton de ludado per fluto.</p>
+			<p>— Kamarado Petrov (mi ne sciis ĉu li nomiĝas Petrov aŭ alie, sed tio ne gravis en la momento) ludos ion per fluto — anoncis mi. La flutisto retiriĝis kaj ekrigardis min ektimigite, sed mi mallaŭte kriis al li, ke ni pagas al la orkestrantoj ne por rigardi la plafonon, sed por ludi. Aŭdante mian koleran voĉon la flutisto konsciigis kaj tuj ekblovis sian instrumenton. En tiu momento alvenis, profunde spirante, alia prizorganto por la amuzo, kaj sciigis al mi, ke en la suba salono la iluziisto Ĉarli atendas min. Mi ordonis al la flutisto, ke li ludu ĝis kiam mi revenis kaj rapide iris al la suba salono. Sed intertempe la iluziisto estis perdinta paciencon kaj mem ekiris serĉi min. Tial estis necese, ke mi trovu eliron el tiu situacio. La publiko ja estis okupinta la lokojn kaj streĉe atendis. Konsiderante rapide kion fari, mi sciiĝis, ke ankaŭ mi scipovas artifikaĵojn kaj mi prenis la kravaton de la kolo de unu sinjoro, prenis ankaŭ unu tre grandan restoracian teleron kaj k-r-a-ŝ! — frapis la teleron je la plankon kaj la kravaton disŝiris je pecoj. La publiko komencis furioze aplaŭdi, nur la direktoro de la restoracio kaj la sinjoro restinta sen kravato ekrigardis min scivoleme — evidente i1i atendis, ke mi tuj rekreos antaŭ ĉies okuloj la teleron kaj la kravaton. Sed ilin mi trankviligis:</p>
+			<p>— Civitanoj — diris mi — vi certe atendas, ke mi restarigos tion, kion mi rompis kaj ŝiris. Vere iuj iluziistoj faras tion, sed mi ne. Mi iras laŭ propra vojo.</p>
+			<p>La publiko denove ekridis kontenta, ĉar tio vere estis tre ridinda, nur la direktoro protestis laŭte kaj ekiris serĉi la ĉefan respondeculon, kaj la posedanto de la kravato sin ĵetis kontraŭ min por bati min. Mi tamen lerte manovris kaj sukcesis eviti la frapon. En tiu momento alvenis la vera iluziisto kaj li flustris en mian orelon, ke li, se ne estus la publiko, deŝirus mian kapon, ĉar li kuris tra ĉiuj salonoj serĉante min. Mi tamen ne konfuziĝis pro tiuj minacoj kaj alproksimiĝante al la rekvizitoj de la iluziisto komencis malfermi skatolojn, korbojn kaj sakojn. Sed subite mi ekkriis, ĉar iu besteto mordis min kaj mi retiriĝante malantaŭen, falis en korbon, el kiu eliris 10 kolomboj, 17 blankaj musoj, 3 kolubroj, ĉirkaŭ 10 akvaj serpentoj kaj multaj aliaj etaj kaj nedanĝeraj bestetoj, kiuj tuj ekrampis inter la gastoj. La lastaj, mi ne scias pro kio, levis grandan bruon; iuj surtabliĝis kaj fariĝis tia tumulto, kvazaŭ okazus io eksterordinara. Al mi mem ĉio ŝajnis tre gaja kaj mi ridis elkore ĝis mi vidis, ke venas la ĉefa respondeculo, kiu havis teruran esprimon sur sia vizaĝo. Li ordonis al mi, ke mi tuj iru supren kaj liberigu la kompatindan flutiston. Mi plenumis tiun ordonon kaj alveninte en la supran salonon mi kun miro konstatis, ke la tuta publiko sidas kiel rigida kaj la flutisto daŭrigas blovadi en la fluton kun lastaj fortoj. Vidante min, li apenaŭ povis prononci: “Ah, jen vi finfine”, kaj svenante falis en la manojn de la ceteraj orkestrantoj, kiuj lin elportis.</p>
+			<p>Por serenigi la atmosferon mi komencis rakonti ian anekdoton, sed mi ne povis fini ĝin, ĉar en la salonon ŝtorme eniris la gastoj el la suba salono kaj post ili inter la tabloj ekrampis la musoj, kunikloj, serpentoj, kolubroj, ratoj k.a. bestoj de la iluziisto Ĉarli Gicov. Okazis vera paniko, en kiu, kiel mi sukcesis vidi, la respondeculo svenis en la brakoj de unu dika sinjorino, kiu siavice konsideris lin muso kaj ekkriis per laŭta voĉo. Sia krio infektis ĉiujn ceterajn kaj baldaŭ la tuta salono eksimilis al vivanta maro, en kiu viroj kaj virinoj interpuŝante unu la alian, renversante kaj rompante ĉion sur sia vojo, direktiĝis al la eliro.</p>
+			<p>Mi plifortigis la tumulton imitante per la mikrofono la voĉojn de diversaj sovaĝaj bestoj, de jaguaro ĝis bengala tigro. Do, persone por mi, la amuzo iris tre gaje kaj plene diferencis de aliaj tiaspecaj aranĝoj.</p>
+			<p>La postan tagon, tamen, vokis min la ĉefa respondeculo kaj klarigis al mi, ke li sugestis al mia ĉefo, ke sentaŭguloj kiel mi ne devas okupi ŝtatan postenon. Poste li daŭrigis:</p>
+			<p>— Krome, “Kara amiko”, je via konto ni kalkulis ĉiujn elspezojn por la amuzvespero, inkluzive la rompitajn kaj difektitajn objektojn je sumo 15 624 levoj. Kaj nun — konkludis li — for de ĉi tie, ĉar semi vidos vin ĉi tie refoje, ml sufokos vin per miaj propraj manoj.</p>
+			<p>Dirante tion li ekĝemis “Oh” kaj kaptis sin je la okulo, kiu estis bluiĝinta — kredeble, la dika sinjorino frapis lin je tiu loko, opiniante lin muso.</p>
+			<p>Sed, “se”… Mi mem intencis forlasi tiun filistran urbon kaj translokiĝi en la ĉefurbon, por tie plene dediĉi min al la arto.</p>
+			<p>Kaj se la vizitantoj de la amuzvespero ne komprenis, ke mi estas la plej originala kaj kuraĝa konferanseo, ja vi karaj legantoj plene komprenis tion. Mi petas vin, kunhelpu, ke mi estu enoficigita kiel ŝtata konferanseo en Sofia. Skribu al la adreso: Vasil Conev, Sofia, Str. 6 septembro N 49.</p>
+
+			<p><b>Vasil Conev</b>  el la bulgara: <b>Metodi Panĉev</b> (el <i>Belarto</i>)</p>
+		</div>
+
+		<?php 
+		} // fin section 1
+		if ($section=="2") {
+		?>
+
+
+	<div class="vortlisto row">
+		<div class="col s12 m6">
+			<p><span class="eo">aktoro</span>: teatra ludisto.</p>
+			<p><span class="eo">almozo</span>: monhelpo al malriĉulo.</p>
+			<p><span class="eo">anekdoto</span>: mallonga sprita rakonto.</p>
+			<p><span class="eo">aplaŭdi</span>: montri aprobon.</p>
+			<p><span class="eo">artifiko</span>: trompa ruzaĵo.</p>
+			<p><span class="eo">bengala</span>: rilata al Bengalujo, orienta regiono de Hindujo.</p>
+			<p><span class="eo">cigano</span>: homo el vaganta hindujdevena gento.</p>
+			<p><span class="eo">civito</span>: tutaĵo de la regnanoj.</p>
+			<p><span class="eo">direktoro</span>: direktisto de ofico.</p>
+			<p><span class="eo">dorno</span>: pikileto de vegetaĵo.</p>
+			<p><span class="eo">evidenta</span>: klara kaj senduba.</p>
+			<p><span class="eo">filistro</span>: senidealulo, vulgarulo.</p>
+			<p><span class="eo">gratuli</span>: esprimi al iu kontentecon pri iu lia sukceso.</p>
+			<p><span class="eo">ĝemi</span>: aŭdigi plendan brueton.</p>
+			<p><span class="eo">iluzio</span>: erara penso aŭ opinio.</p>
+			<p><span class="eo">imiti</span>: similigi sin aŭ ion al io aŭ iu alia.</p>
+			<p><span class="eo">infekti</span>: havigi al iu malsanon.</p>
+			<p><span class="eo">inkluziva</span>: enhavanta en si.</p>
+			<p><span class="eo">instrumento</span>: speciala ilo; ĉi tie</span>: muzikilo.</p>
+			<p><span class="eo">jaguaro</span>: amerika katsimila rabobesto.</p>
+			<p><span class="eo">kolombo</span>: birdo kun forta hejmiga instinkto.</p>
+			<p><span class="eo">kolubro</span>: nedanĝera serpento.</p>
+			<p><span class="eo">kompromiti</span>: endanĝerigi.</p>
+			<p><span class="eo">konferanseo</span>: programestro.</p>
+		</div>
+		<div class="col s12 m6">			
+			<p><span class="eo">konkludo</span>: fino de rezonado; finrezulto.</p>
+			<p><span class="eo">korbo</span>: portebla ujo farita el sekaj herbaĵoj ks.</p>
+			<p><span class="eo">kravato</span>: ornama rubandaĵo, kiun viroj portas ĉirkaŭ la kolo.</p>
+			<p><span class="eo">kroĉi</span>: ligi per hoko.</p>
+			<p><span class="eo">levo</span>: bulgara monunuo.</p>
+			<p><span class="eo">mantelo</span>: longa supervesto.</p>
+			<p><span class="eo">mikrofono</span>: instrumento, kiu plilaŭtigas sonojn.</p>
+			<p><span class="eo">mordi</span>: ŝiri per la dentoj.</p>
+			<p><span class="eo">muso</span>: malgranda malutila besto.</p>
+			<p><span class="eo">neglekti</span>: malzorgi, malatenti.</p>
+			<p><span class="eo">originala</span>: ne simila al aliaj.</p>
+			<p><span class="eo">popo</span>: grekortodoksa pastro.</p>
+			<p><span class="eo">programo</span>: la enhavo de teatraĵo ks.</p>
+			<p><span class="eo">prononci</span>: aŭdigi klare.</p>
+			<p><span class="eo">protesti</span>: fari deklaron kontraŭ io.</p>
+			<p><span class="eo">rato</span>: malutila besteto.</p>
+			<p><span class="eo">rekvizito</span>: teatra ekipo.</p>
+			<p><span class="eo">telegramo</span>: komuniko rapide transsendata.</p>
+			<p><span class="eo">telero</span>: malprofunda manĝovazo.</p>
+			<p><span class="eo">tigro</span>: granda katsimila rabobesto.</p>
+			<p><span class="eo">tikli</span>: delikate kaj ridige tuŝeti iun.</p>
+			<p><span class="eo">treti</span>: piedpremi.</p>
+			<p><span class="eo">tumulto</span>: konfuza brua malordego.</p>
+			<p><span class="eo">vosto</span>: postpenda hararo de besto.</p>
+		</div>
+	</div>
+
+<?php 
+		} // fin section 2
+		if ($section=="3") {
+			getEkzercon(221,$persono_id,"eo");
+				
+		} 
+		if ($section=="4") {
+			getEkzercon(222,$persono_id,"eo");
+			getEkzercon(223,$persono_id,"eo");
+			getEkzercon(224,$persono_id,"eo");
+			getEkzercon(225,$persono_id,"eo");
+		} 
+		if ($section=="5") {
+			getEkzercon(226,$persono_id,"eo");
+		} // envoi de la leçon
+		if ($section=="6") {
+			recapitulatif_lecon_avant_envoi('PP',$leciono,$persono_id);
+		} // fin section 6
+		?>
+		</section>
+		
+		<section id="leciono-fino">
+			<div id="marko" class="right-align">
+				<?php getBoutonFinSection('PP',$leciono,$section,$persono_id); ?>
+				<a id="farita" class="btn-floating btn-large light-blue darken-1 hide"><i class="material-icons">done_all</i></a>
+			</div>
+			<div class="ligoj">
+				<?php getLecioneroAntauxa('PP',$leciono,$section); ?>
+				<?php getLecioneroVenonta('PP',$leciono,$section); ?>
+			</div>
+		</section>
+		
+	</article>
+	
+	<?php include "pppflanko.inc.php"; ?>
+
+</div>
+
+<?php include "ppppiedo.inc.php"; ?>			

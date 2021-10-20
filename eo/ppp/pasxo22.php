8dc86b82403762e687e563c84f568bf829d72d77
Emmanuelle Richard <emmrichard@gmail.com>
2021-10-20 19:55:13 +0200

    pasho 22

---
+++ eo/ppp/pasxo22.php
@@ -0,0 +1,98 @@
+<?php 
+$titolo="Paŝo 21 (dudekunua)";
+$leciono = 21;
+$section=isset($_GET["section"])?htmlspecialchars($_GET["section"]):"1";
+include "pppkapo.inc.php";
+?>
+<div class="row">
+	<article class="col s12 m10 l7 offset-m1 offset-l1">
+		
+		<section id="leciono-enhavo">
+		<?php 
+		getTitoloLecionero('PP',$leciono,$section);
+		?>
+		<?php
+		if ($section=="1") {
+		?>
+
+		<div class="rakonto">
+			<h3>VORTOJ DE ZAMENHOF</h3>
+			<h4>(1) Pri Patriotismo</h4>
+			<p>La dua kulpigo, kiun ni ofte devas aŭdi, estas tio, ke ni esperantistoj estas malbonaj patriotoj. Ĉar tiuj esperantistoj, kiuj traktas la esperantismon kiel ideon, predikas reciprokan justecon kaj fratecon inter la popoloj, kaj ĉar laŭ la opinio de la gentaj ŝovinistoj patriotismo konsistas en malamo kontraŭ ĉio, kio ne estas nia, tial ni laŭ ilia opinio estas malbonaj patriotoj, kaj ili diras, ke la esperantistoj ne amas sian patrujon. Kontraŭ tiu ĉi mensoga, malnobla kaj kalumnia kulpigo ni protestas plej energie, ni protestas per ĉiuj fibroj de nia koro! Dum la pseŭdo-patriotismo, t.e. la genta ŝovinismo, estas parto de tiu komuna malamo, kiu ĉion en la mondo detruas, la vera patriotismo estas parto de tiu granda tutmonda amo, kiu ĉion konstruas, konservas kaj feliĉigas. La esperantismo, kiu predikas amon, kaj la patriotismo, kiu ankaŭ predikas amon, neniam povas esti malamikaj inter si. Ĉiu povas paroli al ni pri ĉiuspeca amo, kaj ni kun danko lin aŭskultos; sed kiam pri amo al la patrujo parolas al ni ŝovinistoj, tiuj reprezentantoj de abomeninda malamo, tiuj mallumaj demonoj, kiuj ne sole inter la landoj, sed ankaŭ en sia propra patrujo konstante instigas homon kontraŭ homo — tiam ni kun la plej granda indigno nin deturnas. Vi, nigraj semantoj de malpaco, parolu nur pri malamo al ĉio, kio ne estas via, parolu pri egoismo, sed neniam uzu la vorton “amo”, ĉar en via buŝo la sankta vorto “amo” malpuriĝas.</p>
+			<p>Vi staras nun antaŭ miaj okuloj, mia kara Litovujo, mia malfeliĉa patrujo, kiun mi neniam povas forgesi, kvankam mi forlasis vin kiel juna knabo. Vi, kiun mi ofte vidas en miaj sonĝoj, vi, kiun nenia alia parto de la tero iam povos anstataŭi en mia koro, vi atestu, kiu vin pli multe, pli kore kaj pli sincere amas: ĉu mi, idea esperantisto, kiu revis pri frateco inter ĉiuj viaj loĝantoj, kvankam mi devis bedaŭrinde forlasi vin, simile al multaj centoj da miloj da aliaj viaj filoj — aŭ ĉu tiuj personoj, kiuj deziras, ke vi apartenu nur al ili, kaj ĉiuj aliaj viaj filoj estu rigardataj kiel fremduloj aŭ sklavoj! Ho, patriotismo, patriotismo, kiam fine la homoj lernos kompreni ĝuste vian sencon! Kiam via sankta nomo ĉesos esti armilo en la manoj de diversaj malhonestuloj! Kiam fine ĉiu homo ricevos la rajton kaj la eblon algluiĝi per sia tuta koro al tiu peco da tero, kiu lin naskis!</p>
+<p>Longe daŭros ankoraŭ malluma nokto sur la tero, sed ne eterne ĝi daŭros. Venos iam la tempo, kiam la homoj ĉesos esti lupoj unuj kontraŭ la aliaj, perforte altrudi al si reciproke siajn lingvojn kaj morojn, ili vivos inter si pace kaj frate, en plena interkonsento ili laboros sur la tero, sur kiu ili vivas, kaj kontraŭ tiuj krudaj fortoj de la naturo, kiuj ilin ĉiujn egale atakas. Kaj kune kaj interkonsente ili celados ĉiuj al unu vero, al unu feliĉo. Kaj se iam venos tiu feliĉa tempo, ĝi estos la frukto de konstanta kaj senlaca laborado de tiuj homoj, kiujn ni vidas nun en ĉi tiu ĉambrego kaj kies nomo, ankoraŭ tre malmulte konata kaj tre malmulte ŝatata, estas “esperantistoj”.</p>
+			<p><i>(el <i>Parolado en la Guildhall</i>, London)</i></p>
+			<h4>(2) Pri la Neceso Paroli Esperanton</h4>
+			<p>Vi eble miros, ke mi parolas al vi ne ruse, sed esperante; vi eble diros, ke ĉar ni havas nun kongreson de samregnanoj kaj ĉiuj, aŭ almenaŭ preskaŭ ĉiuj ĝiaj partoprenantoj komprenas tre bone la saman lingvon, estas multe pli nature paroli al ili en tiu lingvo. Ekzistas tamen gravaj kaŭzoj, pro kiuj mi elektis por mia parolo tiun lingvon, por kiu ni batalas kaj por kiu ni kunvenis.</p>
+			<p>Niaj kongresoj, ne sole la universalaj, sed ankaŭ la naciaj, havas antaŭ ĉio signifon instruan kaj edukan. Esperantistoj, disĵetitaj en diversaj urboj kaj urbetoj, kunvenas en pli aŭ malpli granda amaso, por aŭdi nian lingvon, por kontroli, ĉu ili ĝuste ellernis la lingvon, ĉu ili bone ĝin komprenas, por kompari sian propran manieron de parolado kun la parolmaniero de pli spertaj esperantistoj. Kiam ili poste revenas hejmen, ili ne sole mem parolas pli pure, sed ili alportas modelon de bona parolado al tiuj, kiuj restis hejme. Tiamaniere la kongresoj reguligas la uzadon de la lingvo, kaj dank’ al niaj kongresoj jam nun oni parolas Esperanton perfekte egale ne sole en la plej malproksimaj anguloj de ĉiu aparta regno, sed en ĉiuj plej malsamaj lokoj de la tuta tera globo. Jam nun, kiam oni aŭdas bonan kaj spertan esperantan oratoron, oni neniel povas diveni, al kiu nacio aŭ lando li apartenas. La plena aŭtonomia vivo de nia lingvo, kun ĝia absolute propra, ne pruntita kaj ne imitita spirito, ĉiam pli kaj pli fortikiĝas tiamaniere, kvazaŭ ĉiuj esperantistoj de la mondo loĝus kune sur unu malgranda peco da tero.</p>
+			<p>Ne malpli grava estas la eduka signifo de la esperantistaj kongresoj. Izolitaj esperantistoj, kiuj neniam havis la eblon praktike apliki tion, kion ili lernis, ofte dubas, ĉu efektive per Esperanto oni povas tute bone interkompreniĝi. Eĉ interne de la esperantistaj grupoj oni ofte ne kuraĝas paroli esperante, oni balbutas, oni preferas paroli en sia nacia lingvo, kaj proporcie al la nekuraĝeco de la parolado aperas ankaŭ nekuraĝeco de propagando, ĉar la esperantistoj-balbutantoj malgraŭvole ne povas liberigi sin de la timo, ke eble tamen Esperanto estas afero pli teoria, ol praktika. Sed kiam la balbutanto venas al kongreso, kie li havas la eblon aŭdi bonajn kaj spertajn esperantistajn oratorojn, kiam li per siaj propraj oreloj kaj okuloj konvinkiĝas, kiel bele kaj flue oni povas paroli en Esperanto, li entuziasmiĝas, li vidas, ke li laboras por io viva kaj vivoplena, li revenas hejmen kun nova kuraĝo kaj energio. Niaj kongresoj, ne sole la universalaj, sed ankaŭ la lokaj, tiamaniere edukas konvinkitajn, sekve ankaŭ entuziasmajn batalantojn por nia afero.</p>
+			<p>Tio estas la ĉefaj motivoj, pro kiuj ni en ĉiuj niaj kongresoj ne sole en la universalaj, sed ankaŭ en la naciaj aŭ regionaj, nepre devas paroli ne sole pri Esperanto, sed ankaŭ per Esperanto.</p>
+
+			<p><b>L. L. Zamenhof</b> (el <i>Parolado en St. Petersburg</i>, 1910)</p>
+		</div>
+
+		<?php 
+		} // fin section 1
+		if ($section=="2") {
+		?>
+
+	<div class="vortlisto row">
+		<div class="col s12 m6">
+			<p><span class="eo">aŭtonomio</span>: politika sendependeco.</p>
+			<p><span class="eo">fibro</span>: kunplektitaj korpaj aŭ vegetaĵaj fadenoj.</p>
+			<p><span class="eo">fortika</span>: malfacile rompebla.</p>
+			<p><span class="eo">izoli</span>: apartigi de la aliaj.</p>
+			<p><span class="eo">kalumnii</span>: malutili al iu per mensogaj akuzoj.</p>
+			<p><span class="eo">Litovujo</span>: provinco en tiama rusia Pollando.</p>
+			<p><span class="eo">moro</span>: ĝenerala vivmaniero; kutima konduto.</p>
+		</div>
+		<div class="col s12 m6">			
+			<p><span class="eo">motivo</span>: kialo.</p>
+			<p><span class="eo">oratoro</span>: homo, kiu publike paroladas.</p>
+			<p><span class="eo">prediki</span>: fari religian aŭ kvazaŭreligian paroladon.</p>
+			<p><span class="eo">rusa</span>: rilata al Ruslando.</p>
+			<p><span class="eo">ŝovinisto</span>: fanatika patrioto.</p>
+			<p><span class="eo">teorio</span>: supozo rezulte de rezonado.</p>		
+		</div>
+	</div>
+
+<?php 
+		} // fin section 2
+		if ($section=="3") {
+			getEkzercon(227,$persono_id,"eo");
+				
+		} 
+		if ($section=="4") {
+			getEkzercon(228,$persono_id,"eo");
+			getEkzercon(229,$persono_id,"eo");
+			getEkzercon(230,$persono_id,"eo");
+			getEkzercon(231,$persono_id,"eo");
+		} 
+		if ($section=="5") {
+			getEkzercon(232,$persono_id,"eo");
+			getEkzercon(233,$persono_id,"eo");
+		} // envoi de la leçon
+		if ($section=="6") {
+			recapitulatif_lecon_avant_envoi('PP',$leciono,$persono_id);
+		} // fin section 6
+		?>
+		</section>
+		
+		<section id="leciono-fino">
+			<div id="marko" class="right-align">
+				<?php getBoutonFinSection('PP',$leciono,$section,$persono_id); ?>
+				<a id="farita" class="btn-floating btn-large light-blue darken-1 hide"><i class="material-icons">done_all</i></a>
+			</div>
+			<div class="ligoj">
+				<?php getLecioneroAntauxa('PP',$leciono,$section); ?>
+				<?php getLecioneroVenonta('PP',$leciono,$section); ?>
+			</div>
+		</section>
+		
+	</article>
+	
+	<?php include "pppflanko.inc.php"; ?>
+
+</div>
+
+<?php include "ppppiedo.inc.php"; ?>			

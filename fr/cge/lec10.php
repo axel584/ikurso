011e954678fe53a028956262e1192d050f158827
axel584 <axel584@gmail.com>
2022-02-15 16:22:18 +0100

    rectification de la consigne pour les audio

--- fr/cge/lec10.php
+++ fr/cge/lec10.php
@@ -22,17 +22,17 @@ include "dlekkapo.inc.php";
 
 			<p>
 			Vous voilà rendu à la 10<sup>e</sup> et dernière leçon de votre cours. Elle
-			consistera en un examen de niveau&nbsp;A1 qui nous permettra d’évaluer vos connaissances de l’espéranto écrit. Votre tuteur le corrigera comme
+			consistera en un test de niveau&nbsp;A1 qui nous permettra d’évaluer vos connaissances de l’espéranto écrit. Votre tuteur le corrigera comme
 			d’habitude et vous recevrez un <i>atestilo</i> (attestation de fin de cours).
 			</p>
-			<p>Cet examen est entièrement en espéranto. Il comprend une introduction expliquant le déroulement de l’examen et les notes à obtenir, et deux parties&nbsp;: </p>
+			<p>Ce test est entièrement en espéranto. Il comprend une introduction expliquant le déroulement du test et les notes à obtenir, et trois parties&nbsp;: </p>
 			<ul class="klarigo">
 				<li>Une partie de compréhension orale (<i>Parola komprenado</i>) d’une durée de 20 minutes. Notée sur 25 points.</li>
 				<li>Une partie de compréhension écrite (<i>Skriba komprenado</i>) d’une durée de 30 minutes. Notée sur 25 points.</li>
-				<li>Une partie production écrite (<i>Skriba produktado</i>) d'une durée de 30 minutes. Noté sur 25 points.</li>
+				<li>Une partie production écrite (<i>Skriba produktado</i>) d'une durée de 30 minutes. Notée sur 25 points.</li>
 			</ul>
 			<p>Il convient donc d’avoir environ <b>une heure trente</b> de temps disponible devant vous pour pouvoir lire les explications et effectuer cet examen. Nous vous déconseillons de le faire en plusieurs fois (même si cela est techniquement possible), car cela fausserait le résultat.</p>
-			<p>L’usage du dictionnaire est interdit (que ce soit en ligne ou papier). Rechercher des informations dans le reste du cours est également interdit. Bien sûr, nous ne pouvons pas techniquement vous empêcher de le faire, mais la fiabilité de la note que vous obtiendrez dépendra de votre respect des consignes. De la même façon, les enregistrements audio sont conçus pour être écoutés une seule fois. Vous entendrez le texte une première fois. Cela sera suivi d’une pause de 30&nbsp;secondes puis vous entendrez le texte une deuxième fois (sans que vous ayez besoin de relancer la lecture). En respectant les consignes, vous vous assurerez d’une meilleure estimation de votre niveau.
+			<p>L’usage du dictionnaire est interdit (que ce soit en ligne ou papier). Rechercher des informations dans le reste du cours est également interdit. Bien sûr, nous ne pouvons pas techniquement vous empêcher de le faire, mais la fiabilité de la note que vous obtiendrez dépendra de votre respect des consignes. De la même façon, les enregistrements audio sont conçus pour n'être écoutés que deux fois. Veuillez d'abord lire les questions, écoutez une première fois, répondez aux questions dont vous connaissez la réponse, puis écoutez une deuxième fois afin de trouver les réponses qui vous manquent et vérifier vos réponses déjà écrites. En respectant les consignes, vous vous assurerez d’une meilleure estimation de votre niveau.
 			</p>
 			<p>Il est possible que vous ne connaissiez pas la réponse à certaines questions, que vous ne connaissiez pas certains mots ou que vous ne compreniez pas certains mots dans les enregistrements. Cela est tout à fait normal et il ne faut pas hésiter à passer certaines questions pour y revenir plus tard. Pour réussir l’examen, vous devez obtenir 50&nbsp;% des points sur l’ensemble des parties et au moins 5&nbsp;points dans chacune des parties.<br/>Pour la partie de compréhension orale, nous vous conseillons de lire une première fois les questions avant de lancer l’enregistrement audio.<br/>Vous pouvez bien sûr vous munir d’une feuille de papier brouillon pour prendre des notes pendant l’enregistrement.
 			</p>
@@ -46,9 +46,9 @@ include "dlekkapo.inc.php";
 	<tbody>
 		<tr><td><b>Parola komprenado</b>:<br/>Respondo al demandaro pri tri aŭ kvar tre mallongaj dokumentoj registritaj, kiuj temas pri ĉiutagaj situacioj. (du aŭskultoj)<br/>Maksimuma daŭro de la aŭskultotaj dokumentoj: 3 minutoj</td>
 		<td>Proksimume 20&nbsp;minutoj </td><td>/25</td></tr>
-<tr><td><b>Pkriba komprenado</b>:<br/>Respondo al demandaro pri tri aŭ kvar skribaj dokumentoj kiuj temas pri ĉiutagaj situacioj.</td>
+<tr><td><b>Skriba komprenado</b>:<br/>Respondo al demandaro pri tri aŭ kvar skribaj dokumentoj kiuj temas pri ĉiutagaj situacioj.</td>
 		<td>30&nbsp;minutoj </td><td>/25</td></tr>
-	<tr><td><b>PSkriba produktado</b> :<br/>Ekzamen-objekto el du partoj:<br/>- Kompletigu slipon, formularon, dialogon…<br/>- Redaktu per simplaj frazoj poŝtkarton, mesaĝon, tekston… pri temoj de ĉiutaga vivo.</td>
+	<tr><td><b>Skriba produktado</b> :<br/>Ekzamen-objekto el du partoj:<br/>- Kompletigu slipon, formularon, dialogon…<br/>- Redaktu per simplaj frazoj poŝtkarton, mesaĝon, tekston… pri temoj de ĉiutaga vivo.</td>
 		<td>30 minutoj </td><td>/25</td></tr>
 	<tr><td><b>Sumo</b>:</td><td>1&nbsp;horo kaj 30&nbsp;minutoj </td><td>/75</td></tr>		
 </tbody>

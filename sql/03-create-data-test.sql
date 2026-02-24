a5cd155a3d9b88f5e7b3b8100ba325ea0389f5d9
Axel Rousseau <axel584@gmail.com>
2026-02-24 14:45:09 +0100

    mise à jour des données de tests

--- sql/03-create-data-test.sql
+++ sql/03-create-data-test.sql
@@ -29,10 +29,29 @@ INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALU
 INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 4,0);
 INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 5,0);
 INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 6,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 7,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 8,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 9,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 10,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 11,0);
+INSERT INTO `personoj_lecioneroj` (`persono_id`, `lecionero_id`, `poentoj`) VALUES (@id_lernanto_1, 12,0);
 
-INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 1 ,'la teo estas varma','normaligita',1);
-INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 2 ,'via biskvito estas seka','via biskvito estas seka',1);
-INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 3 ,'via amiko estis bela','via amiko estis bela',1);
-INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 3 ,'la novaj instruistoj estos grandaj','la novaj instruistoj estos grandaj',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 1 ,'La teo estas varma.','normaligita',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 2 ,'Via biskvito estas seka.','via biskvito estas seka',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 3 ,'Via amiko estis bela.','via amiko estis bela',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 3 ,'La novaj instruistoj estos grandaj.','la novaj instruistoj estos grandaj',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 7 ,'Kiuj kukoj estis sekaj ?','kiuj kukoj estis sekaj',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 8 ,'Kie estas la pano ?','kie estas la pano',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 9 ,'Sana knabo estas ĉiam bela.','sana knabo estas cxiam bela',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 10 ,'Ŝi loĝos en granda urbo.','sxi logxos en granda urbo',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 11 ,'La pano estas via.','la pano estas via',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 12 ,'Kiu estas la nova instruisto ?','kiu estas la nova instruisto',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 13 ,'Jen biskvitoj. Kiuj estas bonaj ?','jen biskvitoj kiuj estas bonaj',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 14 ,'La biskvito estas malgranda.','la biskvito estas malgranda',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 15 ,'Via filo estas bela.','via filo estas bela',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 16 ,'Via fratino estas malsana.','via fratino estas malsana',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 17 ,'La instruistoj estas grandaj.','la instruistoj estas grandaj',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 18 ,'La urbo estas bela.','la urbo estas bela',1);
+INSERT INTO `respondoj` (`persono_id`, `ekzercero_id`,`respondo`,`normaligita`,`gxusta`) VALUES (@id_lernanto_1, 19 ,'Mi estas lernanto de la kurso.','mi estas lernanto de la kurso',1);
 
 
